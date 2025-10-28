a9cb53e87819691da1b688ef163c44169737fead
Davide <keeshux@gmail.com>
2025-10-28 13:48:56 +0100

    Move some configuration to root
    Because related to app-shared:
    
    - SwiftGen configuration file
    - L10n scripts

--- app-apple/scripts/env.sh
+++ app-apple/scripts/env.sh
@@ -7,5 +7,3 @@ certs_p12_path="/Volumes/Personale/Passepartout/certificates.p12"
 certs_profiles_root=~/Library/Developer/Xcode/UserData/Provisioning\ Profiles
 certs_zip_path="../../notes/certificates.zip"
 certs_tmp_root="$tmp_root/certs"
-translations_input_path="l10n"
-translations_output_path="Package/Sources/AppStrings/Resources"
