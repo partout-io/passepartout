4a1dfbcb77fcce52089c5d6b8f393e371585918a
Davide <keeshux@gmail.com>
2025-11-02 16:09:18 +0100

    Support throwing closure in MainActor.sync

--- app-shared/Sources/CommonUtils/Extensions/MainActor+Sync.swift
+++ app-shared/Sources/CommonUtils/Extensions/MainActor+Sync.swift
@@ -6,17 +6,17 @@ import Foundation
 
 // XXX: Trick to call MainActor code synchronously
 extension MainActor {
-    public static func sync<T>(_ block: @escaping @Sendable @MainActor () -> T) -> T where T: Sendable {
+    public static func sync<T>(_ block: @escaping @Sendable @MainActor () throws -> T) rethrows -> T where T: Sendable {
         guard Thread.isMainThread else {
             var result: T!
             let semaphore = DispatchSemaphore(value: 0)
             Task { @MainActor in
-                result = block()
+                result = try block()
                 semaphore.signal()
             }
             semaphore.wait()
             return result
         }
-        return MainActor.assumeIsolated(block)
+        return try MainActor.assumeIsolated(block)
     }
 }
