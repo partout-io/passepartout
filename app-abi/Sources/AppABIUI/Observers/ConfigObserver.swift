ee05b63d63c3abc4df1b0a394c748cf4095a0c67
Davide <keeshux@gmail.com>
2025-11-08 18:59:01 +0100

    .

--- app-abi/Sources/AppABIUI/Observers/ConfigObserver.swift
+++ app-abi/Sources/AppABIUI/Observers/ConfigObserver.swift
@@ -2,8 +2,6 @@
 //
 // SPDX-License-Identifier: GPL-3.0
 
-// FIXME: ###, ConfigManager
-
 // MARK: - Actions
 //
 //public func refreshBundle() async
--- app-abi/Sources/AppABIUI/Observers/IAPObserver.swift
+++ app-abi/Sources/AppABIUI/Observers/IAPObserver.swift
@@ -2,4 +2,32 @@
 //
 // SPDX-License-Identifier: GPL-3.0
 
-// FIXME: ###, IAPManager
+// MARK: - Actions
+
+//public func enable() async
+//public func purchasableProducts(for products: [AppProduct]) async throws -> [InAppProduct]
+//public func purchase(_ purchasableProduct: InAppProduct) async throws -> InAppPurchaseResult
+//public func restorePurchases() async throws
+//public func reloadReceipt() async
+
+//public func observeObjects(withProducts: Bool = true)
+//public func fetchLevelIfNeeded() async
+
+// MARK: - State
+
+//public var isLoadingReceipt: Bool
+//@Published public var isEnabled = true
+//public private(set) var originalPurchase: OriginalPurchase?
+//public private(set) var purchasedProducts: Set<AppProduct>
+//@Published  public private(set) var eligibleFeatures: Set<AppFeature>
+//@Published private var pendingReceiptTask: Task<Void, Never>?
+
+//public var isBeta: Bool
+//public func isEligible(for feature: AppFeature) -> Bool
+//public func isEligible<C>(for features: C) -> Bool where C: Collection, C.Element == AppFeature
+//public var isEligibleForComplete: Bool
+//public var isEligibleForFeedback: Bool
+//public var isPayingUser: Bool
+//public var didPurchaseComplete: Bool
+//public func didPurchase(_ purchasable: InAppProduct) -> Bool
+//public func didPurchase(_ purchasable: [InAppProduct]) -> Bool
