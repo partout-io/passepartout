87907a489fb2d02f597f00e4d91412ec372f42e1
Davide <keeshux@gmail.com>
2025-11-08 17:50:33 +0100

    TunnelObserver

--- app-abi/Sources/AppABIUI/TunnelObserver.swift
+++ app-abi/Sources/AppABIUI/TunnelObserver.swift
@@ -32,4 +32,23 @@ final class TunnelObserver {
         print("onUpdate() called")
         refresh()
     }
+
+    // MARK: - Actions
+
+//    public func install(_ profile: Profile) async throws
+//    public func connect(with profile: Profile, force: Bool = false) async throws
+//    public func disconnect(from profileId: Profile.ID) async throws
+//    public func currentLog(parameters: Constants.Log) async -> [String]
+
+    // MARK: - State
+
+//    public var activeProfile: TunnelActiveProfile?
+//    public var activeProfiles: [Profile.ID: TunnelActiveProfile]
+//    public var activeProfilesStream: AsyncStream<[Profile.ID: TunnelActiveProfile]>
+//    public func isActiveProfile(withId profileId: Profile.ID) -> Bool
+//    public func status(ofProfileId profileId: Profile.ID) -> TunnelStatus
+//    public func connectionStatus(ofProfileId profileId: Profile.ID) -> TunnelStatus
+//    public func dataCount(ofProfileId profileId: Profile.ID) -> DataCount?
+//    public func lastErrorCode(ofProfileId profileId: Profile.ID) -> PartoutError.Code?
+//    public func value<T>(forKey key: TunnelEnvironmentKey<T>, ofProfileId profileId: Profile.ID) -> T?
 }
