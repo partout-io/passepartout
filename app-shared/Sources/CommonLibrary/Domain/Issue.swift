e1583dc3a5547755318c23e3be1d43ad359a6ed6
Davide <keeshux@gmail.com>
2025-11-13 09:19:57 +0100

    Move more UI domain

---
+++ app-shared/Sources/CommonLibrary/Domain/Issue.swift
@@ -0,0 +1,44 @@
+// SPDX-FileCopyrightText: 2025 Davide De Rosa
+//
+// SPDX-License-Identifier: GPL-3.0
+
+public struct Issue: Identifiable {
+    public let id: UUID
+
+    public let comment: String
+
+    public let appLine: String?
+
+    public let purchasedProducts: Set<AppProduct>
+
+    public let providerLastUpdates: [ProviderID: Timestamp]
+
+    public let appLog: Data?
+
+    public let tunnelLog: Data?
+
+    public let osLine: String
+
+    public let deviceLine: String?
+
+    public init(
+        comment: String,
+        appLine: String?,
+        purchasedProducts: Set<AppProduct>,
+        providerLastUpdates: [ProviderID: Timestamp] = [:],
+        appLog: Data? = nil,
+        tunnelLog: Data? = nil
+    ) {
+        id = UUID()
+        self.comment = comment
+        self.appLine = appLine
+        self.purchasedProducts = purchasedProducts
+        self.appLog = appLog
+        self.tunnelLog = tunnelLog
+        self.providerLastUpdates = providerLastUpdates
+
+        let systemInfo = SystemInformation()
+        osLine = systemInfo.osString
+        deviceLine = systemInfo.deviceString
+    }
+}
