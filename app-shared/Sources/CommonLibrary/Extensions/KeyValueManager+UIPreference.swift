295adaeb9ccd6664739d353ac2c04ee5eb4735c8
Davide <keeshux@gmail.com>
2025-11-13 18:19:10 +0100

    Move something more from AppLibrary
    - Profile export extensions
    - UIPreference extensions of KeyValueManager

--- app-shared/Sources/CommonLibrary/Extensions/KeyValueManager+UIPreference.swift
+++ app-shared/Sources/CommonLibrary/Extensions/KeyValueManager+UIPreference.swift
@@ -2,8 +2,6 @@
 //
 // SPDX-License-Identifier: GPL-3.0
 
-import CommonLibrary
-
 extension KeyValueManager {
     public func object<T>(forUIPreference pref: UIPreference) -> T? {
         object(forKey: pref.key)
--- app-shared/Sources/CommonLibrary/Extensions/Profile+Extensions.swift
+++ app-shared/Sources/CommonLibrary/Extensions/Profile+Extensions.swift
@@ -9,3 +9,23 @@ extension Profile {
         lhs.name.lowercased() < rhs.name.lowercased()
     }
 }
+
+extension Profile {
+    public var defaultFilename: String {
+        name.appending(".json")
+    }
+
+    public func writeToJSON(coder: RegistryCoder) throws -> String {
+        try coder.json(from: self)
+    }
+
+    public func writeToURL(coder: RegistryCoder) throws -> URL {
+        let json = try writeToJSON(coder: coder)
+        let data = Data(json.utf8)
+        let url = FileManager.default.temporaryDirectory
+            .appendingPathComponent(id.uuidString)
+            .appendingPathExtension("json")
+        try data.write(to: url, options: .atomic)
+        return url
+    }
+}
