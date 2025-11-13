2b20459bbe437b8c15df1298a7c5241b1e303336
Davide <keeshux@gmail.com>
2025-11-13 17:11:41 +0100

    Split shared entities

--- app-shared/Sources/CommonResources/API+Shared.swift
+++ app-shared/Sources/CommonResources/API+Shared.swift
@@ -4,33 +4,6 @@
 
 import CommonLibrary
 import CommonProviders
-import Partout
-
-public enum Resources {
-    public static let constants = Bundle.module.unsafeDecode(Constants.self, filename: "Constants")
-
-    public static let credits = Bundle.module.unsafeDecode(Credits.self, filename: "Credits")
-
-    public static let issueTemplate: String = {
-        do {
-            guard let templateURL = Bundle.module.url(forResource: "Issue", withExtension: "txt") else {
-                fatalError("Unable to find Issue.txt in Resources")
-            }
-            return try String(contentsOf: templateURL)
-        } catch {
-            fatalError("Unable to parse Issue.txt: \(error)")
-        }
-    }()
-
-#if os(tvOS)
-    public static let webUploaderPath: String = {
-        guard let path = Bundle.module.path(forResource: "web_uploader", ofType: "html") else {
-            fatalError("Unable to find web_uploader.html in Resources")
-        }
-        return path
-    }()
-#endif
-}
 
 extension API {
     public static var shared: [APIMapper] {
---
+++ app-shared/Sources/CommonResources/Resources.swift
@@ -0,0 +1,31 @@
+// SPDX-FileCopyrightText: 2025 Davide De Rosa
+//
+// SPDX-License-Identifier: GPL-3.0
+
+import CommonLibrary
+
+public enum Resources {
+    public static let constants = Bundle.module.unsafeDecode(Constants.self, filename: "Constants")
+
+    public static let credits = Bundle.module.unsafeDecode(Credits.self, filename: "Credits")
+
+    public static let issueTemplate: String = {
+        do {
+            guard let templateURL = Bundle.module.url(forResource: "Issue", withExtension: "txt") else {
+                fatalError("Unable to find Issue.txt in Resources")
+            }
+            return try String(contentsOf: templateURL)
+        } catch {
+            fatalError("Unable to parse Issue.txt: \(error)")
+        }
+    }()
+
+#if os(tvOS)
+    public static let webUploaderPath: String = {
+        guard let path = Bundle.module.path(forResource: "web_uploader", ofType: "html") else {
+            fatalError("Unable to find web_uploader.html in Resources")
+        }
+        return path
+    }()
+#endif
+}
