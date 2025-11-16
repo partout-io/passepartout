6bbd03c9d29f446050efb63d0dda993b3f57bcb0
Davide <keeshux@gmail.com>
2025-11-16 10:44:46 +0100

    Merge Partout-related localizations into single file
    Plus module-specific in "Modules" folder.

--- app-shared/Sources/AppLibrary/L10n/Modules/OnDemandModule+L10n.swift
+++ app-shared/Sources/AppLibrary/L10n/Modules/OnDemandModule+L10n.swift
@@ -3,3 +3,19 @@
 // SPDX-License-Identifier: GPL-3.0
 
 import Foundation
+import Partout
+
+extension OnDemandModule.Policy: LocalizableEntity {
+    public var localizedDescription: String {
+        switch self {
+        case .any:
+            return Strings.Entities.OnDemand.Policy.any
+        case .excluding:
+            return Strings.Entities.OnDemand.Policy.excluding
+        case .including:
+            return Strings.Entities.OnDemand.Policy.including
+        @unknown default:
+            return Strings.Entities.OnDemand.Policy.any
+        }
+    }
+}
