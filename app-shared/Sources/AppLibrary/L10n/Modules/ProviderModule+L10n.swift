67a7fac33219cc38a17dd25c0ffe55ecbfaa08ba
Davide <keeshux@gmail.com>
2025-11-16 12:13:14 +0100

    Fix gitignored case-insensitive "L10n" folder

---
+++ app-shared/Sources/AppLibrary/L10n/Modules/ProviderModule+L10n.swift
@@ -0,0 +1,70 @@
+// SPDX-FileCopyrightText: 2025 Davide De Rosa
+//
+// SPDX-License-Identifier: GPL-3.0
+
+import CommonLibrary
+import Partout
+
+extension ProviderID: CustomDebugStringConvertible {
+    public var debugDescription: String {
+        rawValue
+    }
+}
+
+extension ProviderEntity: LocalizableEntity {
+    public var localizedDescription: String {
+        heuristic?.localizedDescription ?? server.localizedDescription
+    }
+}
+
+extension ProviderHeuristic: LocalizableEntity {
+    public var localizedDescription: String {
+        switch self {
+        case .exact(let server):
+            return server.localizedDescription
+        case .sameCountry(let countryCode):
+            return countryCode.localizedAsRegionCode ?? countryCode
+        case .sameRegion(let region):
+            return region.localizedDescription
+        }
+    }
+}
+
+extension ProviderRegion: LocalizableEntity {
+    public var localizedDescription: String {
+        [countryCode.localizedAsRegionCode, area]
+            .compactMap { $0 }
+            .joined(separator: ", ")
+    }
+}
+
+extension ProviderServer: LocalizableEntity {
+    public var localizedDescription: String {
+        [metadata.countryCode.localizedAsRegionCode, metadata.area]
+            .compactMap { $0 }
+            .joined(separator: ", ")
+    }
+}
+
+extension ProviderServer {
+    public var localizedCountry: String? {
+        metadata.countryCode.localizedAsRegionCode
+    }
+
+    public var address: String {
+        if let hostname {
+            return hostname
+        }
+        if let ipAddresses {
+            return ipAddresses
+                .compactMap {
+                    guard let address = Address(data: $0) else {
+                        return nil
+                    }
+                    return address.description
+                }
+                .joined(separator: ", ")
+        }
+        return ""
+    }
+}
