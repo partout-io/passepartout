6090254b6d682fbb40d305f2fde13dfe74e98485
Davide <keeshux@gmail.com>
2025-12-06 22:22:01 +0100

    Fix ABI header double inclusion and submodule

--- app-shared/Sources/ABI_C/include/passepartout.h
+++ app-shared/Sources/ABI_C/include/passepartout.h
@@ -4,5 +4,10 @@
  * SPDX-License-Identifier: GPL-3.0
  */
 
+#ifndef __PASSEPARTOUT_H
+#define __PASSEPARTOUT_H
+
 extern int psp_example_sum(int a, int b);
 extern const char *psp_example_json();
+
+#endif
