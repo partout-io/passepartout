partout-io/passepartout

commit 061e3f6fb1790dec89e27cd6e46fb0dddf5448ef

Add link to /Applications dynamically to .dmg

app-apple/ci/dmg-generate.sh
@@ -33,6 +33,9 @@ if [[ -n "$is_template" ]]; then
         -mountpoint "$mnt" \
         -readwrite -noautoopen
 
+    echo "Link to /Applications..."
+    ln -s /Applications "$mnt/Applications"
+
     echo "Reapply .DS_Store..."
     cp "$srcfolder/.DS_Store" "$mnt"
     chmod 644 "$mnt/.DS_Store"

app-apple/ci/dmg/Applications
@@ -1,1 +1,0 @@
-/Applications

commit 73982f6267df0e2032f7e2fe5b4ed9bd7224454f

Add link in .dmg before creating the image
Broken in 061e3f6fb1790dec89e27cd6e46fb0dddf5448ef

Was changing template...

app-apple/ci/dmg-generate.sh
@@ -18,6 +18,9 @@ set -e
 echo "Copy .app to .dmg contents..."
 cp -RH "dist/macOS/$name.app" "$srcfolder"
 
+echo "Add link to /Applications..."
+ln -sf /Applications "$srcfolder/Applications"
+
 if [[ -n "$is_template" ]]; then
     echo "Create template $volname..."
     hdiutil create \
@@ -32,9 +35,6 @@ if [[ -n "$is_template" ]]; then
     hdiutil attach "$dmg.template.dmg" \
         -mountpoint "$mnt" \
         -readwrite -noautoopen
-
-    echo "Link to /Applications..."
-    ln -s /Applications "$mnt/Applications"
 
     echo "Reapply .DS_Store..."
     cp "$srcfolder/.DS_Store" "$mnt"