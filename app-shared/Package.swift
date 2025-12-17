1feaaa199523fb265c65819e7b97a45f156358ce
Davide <keeshux@gmail.com>
2025-12-17 14:57:10 +0100

    Fix and clean up package

--- app-shared/Package.swift
+++ app-shared/Package.swift
@@ -3,13 +3,6 @@
 
 import PackageDescription
 
-let swiftSettings = {
-    var list: [SwiftSetting] = [
-        .define("PSP_CROSS", .when(platforms: [.android, .linux, .windows]))
-    ]
-    return list
-}()
-
 let package = Package(
     name: "app-shared",
     defaultLocalization: "en",
@@ -27,85 +20,83 @@ let package = Package(
     dependencies: [
         .package(path: "../submodules/partout"),
         .package(url: "https://github.com/apple/swift-nio", from: "2.83.0")
-    ],
-    targets: [
-        .executableTarget(
-            name: "CommonExample_C",
-            dependencies: ["CommonLibrary"]
-        ),
-        .target(
-            name: "CommonLibrary",
-            dependencies: [
-                "CommonLibraryCore",
-                .target(name: "CommonLibraryApple", condition: .when(platforms: [.iOS, .macOS, .tvOS]))
-            ],
-            swiftSettings: swiftSettings
-        ),
-        .target(
-            name: "CommonLibraryApple",
-            dependencies: ["CommonLibraryCore"]
-        ),
-        .target(
-            name: "CommonLibraryCore",
-            dependencies: {
-                var list: [Target.Dependency] = [
-                    .product(name: "NIO", package: "swift-nio", condition: .when(platforms: [.tvOS])),
-                    .product(name: "NIOHTTP1", package: "swift-nio", condition: .when(platforms: [.tvOS])),
-                    "partout"
-                ]
-                list.append("CommonLibraryCore_C")
-                if withProviders {
-                    list.append(
-                        .target(name: "CommonProviders")
-                    )
-                }
-                return list
-            }(),
-            swiftSettings: swiftSettings
-        ),
-        .target(
-            name: "CommonLibraryCore_C"
-        ),
-        .testTarget(
-            name: "CommonLibraryTests",
-            dependencies: ["CommonLibrary"],
-            resources: [
-                .process("Resources")
-            ],
-            swiftSettings: swiftSettings
-        )
     ]
 )
 
-if withProviders {
-    package.products.append(
-        .library(
-            name: "CommonProviders",
-            targets: ["CommonProviders"]
-        )
-    )
-    package.targets.append(contentsOf: [
-        .target(
-            name: "CommonProviders",
-            dependencies: ["CommonProvidersAPI"],
-            swiftSettings: swiftSettings
-        ),
-        .target(
-            name: "CommonProvidersAPI",
-            dependencies: ["CommonProvidersCore"],
-            resources: [
-                .copy("JSON")
-            ],
-            swiftSettings: swiftSettings
-        ),
-        .target(
-            name: "CommonProvidersCore",
-            dependencies: ["partout"],
-            swiftSettings: swiftSettings
-        )
-    ])
-}
+let swiftSettings: [SwiftSetting] = [
+    .define("PSP_CROSS", .when(platforms: [.android, .linux, .windows]))
+]
 
+// MARK: Main
+
+package.targets.append(contentsOf: [
+    .target(
+        name: "CommonLibrary",
+        dependencies: [
+            "CommonLibraryCore",
+            .target(name: "CommonLibraryApple", condition: .when(platforms: [.iOS, .macOS, .tvOS]))
+        ],
+        swiftSettings: swiftSettings
+    ),
+    .target(
+        name: "CommonLibraryApple",
+        dependencies: ["CommonLibraryCore"]
+    ),
+    .target(
+        name: "CommonLibraryCore",
+        dependencies: {
+            var list: [Target.Dependency] = [
+                .product(name: "NIO", package: "swift-nio", condition: .when(platforms: [.tvOS])),
+                .product(name: "NIOHTTP1", package: "swift-nio", condition: .when(platforms: [.tvOS])),
+                "partout"
+            ]
+            list.append("CommonLibraryCore_C")
+            list.append("CommonProviders")
+            return list
+        }(),
+        swiftSettings: swiftSettings
+    ),
+    .target(
+        name: "CommonLibraryCore_C"
+    ),
+    .testTarget(
+        name: "CommonLibraryTests",
+        dependencies: ["CommonLibrary"],
+        resources: [
+            .process("Resources")
+        ],
+        swiftSettings: swiftSettings
+    )
+])
+
+// MARK: Providers
+
+package.products.append(
+    .library(
+        name: "CommonProviders",
+        targets: ["CommonProviders"]
+    )
+)
+package.targets.append(contentsOf: [
+    .target(
+        name: "CommonProviders",
+        dependencies: ["CommonProvidersAPI"],
+        swiftSettings: swiftSettings
+    ),
+    .target(
+        name: "CommonProvidersAPI",
+        dependencies: ["CommonProvidersCore"],
+        resources: [
+            .copy("JSON")
+        ],
+        swiftSettings: swiftSettings
+    ),
+    .target(
+        name: "CommonProvidersCore",
+        dependencies: ["partout"],
+        swiftSettings: swiftSettings
+    )
+])
 #if canImport(Darwin)
 package.targets.append(contentsOf: [
     .testTarget(
@@ -121,3 +112,12 @@ package.targets.append(contentsOf: [
     )
 ])
 #endif
+
+// MARK: Examples
+
+package.targets.append(
+    .executableTarget(
+        name: "CommonExample_C",
+        dependencies: ["CommonLibrary"]
+    )
+)
