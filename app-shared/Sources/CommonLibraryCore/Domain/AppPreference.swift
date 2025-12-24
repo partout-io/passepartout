ecaccef23852fe72a7aa89cec6f692ffdad29808
Davide <keeshux@gmail.com>
2025-12-24 23:45:24 +0100

    .

--- app-shared/Sources/CommonLibraryCore/Domain/AppPreference.swift
+++ app-shared/Sources/CommonLibraryCore/Domain/AppPreference.swift
@@ -108,3 +108,35 @@ extension ABI.AppPreferenceValues {
         flags.subtracting(experimental.ignoredConfigFlags)
     }
 }
+
+extension KeyValueStore {
+    // TODO: #1513, refactor to keep automatically in sync with AppPreference
+    public var preferences: ABI.AppPreferenceValues {
+        get {
+            var values = ABI.AppPreferenceValues()
+            values.deviceId = string(forAppPreference: .deviceId)
+            values.dnsFallsBack = bool(forAppPreference: .dnsFallsBack)
+            values.lastCheckedVersionDate = double(forAppPreference: .lastCheckedVersionDate)
+            values.lastCheckedVersion = object(forAppPreference: .lastCheckedVersion)
+            values.lastUsedProfileId = object(forAppPreference: .lastUsedProfileId)
+            values.logsPrivateData = bool(forAppPreference: .logsPrivateData)
+            values.relaxedVerification = bool(forAppPreference: .relaxedVerification)
+            values.skipsPurchases = bool(forAppPreference: .skipsPurchases)
+            values.configFlagsData = object(forAppPreference: .configFlags)
+            values.experimentalData = object(forAppPreference: .experimental)
+            return values
+        }
+        set {
+            set(newValue.deviceId, forAppPreference: .dnsFallsBack)
+            set(newValue.dnsFallsBack, forAppPreference: .dnsFallsBack)
+            set(newValue.lastCheckedVersionDate, forAppPreference: .lastCheckedVersionDate)
+            set(newValue.lastCheckedVersion, forAppPreference: .lastCheckedVersion)
+            set(newValue.lastUsedProfileId, forAppPreference: .lastUsedProfileId)
+            set(newValue.logsPrivateData, forAppPreference: .logsPrivateData)
+            set(newValue.relaxedVerification, forAppPreference: .relaxedVerification)
+            set(newValue.skipsPurchases, forAppPreference: .skipsPurchases)
+            set(newValue.configFlagsData, forAppPreference: .configFlags)
+            set(newValue.experimentalData, forAppPreference: .experimental)
+        }
+    }
+}
--- app-shared/Sources/CommonLibraryCore/Extensions/KeyValueStore+AppPreference.swift
+++ app-shared/Sources/CommonLibraryCore/Extensions/KeyValueStore+AppPreference.swift
@@ -3,38 +3,6 @@
 // SPDX-License-Identifier: GPL-3.0
 
 extension KeyValueStore {
-    // TODO: #1513, refactor to keep automatically in sync with AppPreference
-    public var preferences: ABI.AppPreferenceValues {
-        get {
-            var values = ABI.AppPreferenceValues()
-            values.deviceId = string(forAppPreference: .deviceId)
-            values.dnsFallsBack = bool(forAppPreference: .dnsFallsBack)
-            values.lastCheckedVersionDate = double(forAppPreference: .lastCheckedVersionDate)
-            values.lastCheckedVersion = object(forAppPreference: .lastCheckedVersion)
-            values.lastUsedProfileId = object(forAppPreference: .lastUsedProfileId)
-            values.logsPrivateData = bool(forAppPreference: .logsPrivateData)
-            values.relaxedVerification = bool(forAppPreference: .relaxedVerification)
-            values.skipsPurchases = bool(forAppPreference: .skipsPurchases)
-            values.configFlagsData = object(forAppPreference: .configFlags)
-            values.experimentalData = object(forAppPreference: .experimental)
-            return values
-        }
-        set {
-            set(newValue.deviceId, forAppPreference: .dnsFallsBack)
-            set(newValue.dnsFallsBack, forAppPreference: .dnsFallsBack)
-            set(newValue.lastCheckedVersionDate, forAppPreference: .lastCheckedVersionDate)
-            set(newValue.lastCheckedVersion, forAppPreference: .lastCheckedVersion)
-            set(newValue.lastUsedProfileId, forAppPreference: .lastUsedProfileId)
-            set(newValue.logsPrivateData, forAppPreference: .logsPrivateData)
-            set(newValue.relaxedVerification, forAppPreference: .relaxedVerification)
-            set(newValue.skipsPurchases, forAppPreference: .skipsPurchases)
-            set(newValue.configFlagsData, forAppPreference: .configFlags)
-            set(newValue.experimentalData, forAppPreference: .experimental)
-        }
-    }
-}
-
-extension KeyValueStore {
     public func set<V>(_ object: V?, forAppPreference pref: ABI.AppPreference) {
         set(object, forKey: pref.key)
     }
