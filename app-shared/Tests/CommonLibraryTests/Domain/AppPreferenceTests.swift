partout-io/passepartout

commit 0a50d2369d8974707004fef27c5eaed05773f9e1

Hide key-value stores from UI (#1640)
Access through:

- UserPreferencesObservable in views
- KeyValueStore in ABI

So that the UI is not concerned about the specific key names in e.g.
UserDefaults.

As a design rule:

- UI observables access the key-value store via
UserPreferencesObservable (OnboardingObservable, ...)
- ABI managers access the key-value store directly (ExtendedTunnel,
VersionChecker, ...)

Consequently:

- Preference views act on abstract UserPreferencesObservable bindings
rather than raw keys
- Move key-value preferences from AppearanceObservable and MacSettings
to UserPreferencesObservable
- AppearanceObservable is deleted
- MacSettings no longer needs a key-value store
- Drop KeyValueManager and ThemeKeyValueModifier, redundant

Extra:

- Implement logging at ABI level
- Rename CommonABI to AppABI in sight of a TunnelABI to interpose
between PacketTunnelProvider and the business logic

app-apple/Sources/AppLibrary/Extensions/KeyValueStore+UIPreference.swift
@@ -1,0 +1,31 @@
+// SPDX-FileCopyrightText: 2025 Davide De Rosa
+//
+// SPDX-License-Identifier: GPL-3.0
+
+import CommonLibrary
+
+extension KeyValueStore {
+    func set<V>(_ value: V?, forUIPreference pref: UIPreference) {
+        set(value, forKey: pref.key)
+    }
+
+    func object<V>(forUIPreference pref: UIPreference) -> V? {
+        object(forKey: pref.key)
+    }
+
+    func bool(forUIPreference pref: UIPreference) -> Bool {
+        bool(forKey: pref.key)
+    }
+
+    func integer(forUIPreference pref: UIPreference) -> Int {
+        integer(forKey: pref.key)
+    }
+
+    func double(forUIPreference pref: UIPreference) -> Double {
+        double(forKey: pref.key)
+    }
+
+    func string(forUIPreference pref: UIPreference) -> String? {
+        string(forKey: pref.key)
+    }
+}

app-apple/Sources/AppLibrary/Observables/UserPreferencesObservable.swift
@@ -4,12 +4,143 @@
 
 import CommonLibrary
 import Observation
+import SwiftUI
 
 @MainActor @Observable
 public final class UserPreferencesObservable {
-    private let kvManager: KeyValueManager
+    private let kvStore: KeyValueStore
 
-    public init(kvManager: KeyValueManager) {
-        self.kvManager = kvManager
+    public init(kvStore: KeyValueStore) {
+        self.kvStore = kvStore
+
+        dnsFallsBack = kvStore.bool(forAppPreference: .dnsFallsBack)
+        experimental = kvStore.object(forAppPreference: .experimental) as ABI.AppPreferenceValues.Experimental? ?? ABI.AppPreferenceValues.Experimental()
+        keepsInMenu = kvStore.bool(forUIPreference: .keepsInMenu)
+        lastInfrastructureRefresh = kvStore.object(forUIPreference: .lastInfrastructureRefresh) as [String: TimeInterval]?
+        locksInBackground = kvStore.bool(forUIPreference: .locksInBackground)
+        logsPrivateData = kvStore.bool(forAppPreference: .logsPrivateData)
+        onboardingStep = kvStore.string(forUIPreference: .onboardingStep).flatMap {
+            OnboardingStep(rawValue: $0)
+        }
+        onlyShowsFavorites = kvStore.bool(forUIPreference: .onlyShowsFavorites)
+        pinsActiveProfile = kvStore.bool(forUIPreference: .pinsActiveProfile)
+        profilesLayout = kvStore.string(forUIPreference: .profilesLayout).flatMap {
+            ProfilesLayout(rawValue: $0)
+        } ?? .list
+        relaxedVerification = kvStore.bool(forAppPreference: .relaxedVerification)
+        systemAppearance = kvStore.string(forUIPreference: .systemAppearance).flatMap {
+            SystemAppearance(rawValue: $0)
+        }
+    }
+
+    // MARK: Preferences
+
+    public var dnsFallsBack: Bool {
+        didSet {
+            kvStore.set(dnsFallsBack, forAppPreference: .dnsFallsBack)
+        }
+    }
+
+    public var experimental: ABI.AppPreferenceValues.Experimental {
+        didSet {
+            kvStore.set(experimental, forAppPreference: .experimental)
+        }
+    }
+
+    public var keepsInMenu: Bool {
+        didSet {
+            kvStore.set(keepsInMenu, forUIPreference: .keepsInMenu)
+        }
+    }
+
+    public var lastInfrastructureRefresh: [String: TimeInterval]? {
+        didSet {
+            kvStore.set(lastInfrastructureRefresh, forUIPreference: .lastInfrastructureRefresh)
+        }
+    }
+
+    public var locksInBackground: Bool {
+        didSet {
+            kvStore.set(locksInBackground, forUIPreference: .locksInBackground)
+        }
+    }
+
+    public var logsPrivateData: Bool {
+        didSet {
+            kvStore.set(logsPrivateData, forAppPreference: .logsPrivateData)
+        }
+    }
+
+    public var onboardingStep: OnboardingStep? {
+        didSet {
+            kvStore.set(onboardingStep?.rawValue, forUIPreference: .onboardingStep)
+        }
+    }
+
+    public var onlyShowsFavorites: Bool {
+        didSet {
+            kvStore.set(onlyShowsFavorites, forUIPreference: .onlyShowsFavorites)
+        }
+    }
+
+    public var pinsActiveProfile: Bool {
+        didSet {
+            kvStore.set(pinsActiveProfile, forUIPreference: .pinsActiveProfile)
+        }
+    }
+
+    public var profilesLayout: ProfilesLayout {
+        didSet {
+            kvStore.set(profilesLayout.rawValue, forUIPreference: .profilesLayout)
+        }
+    }
+
+    public var relaxedVerification: Bool {
+        didSet {
+            kvStore.set(relaxedVerification, forAppPreference: .relaxedVerification)
+        }
+    }
+
+    public var systemAppearance: SystemAppearance? {
+        didSet {
+            kvStore.set(systemAppearance?.rawValue, forUIPreference: .systemAppearance)
+            applyAppearance()
+        }
+    }
+}
+
+// MARK: - Appearance
+
+extension UserPreferencesObservable {
+    public func applyAppearance() {
+#if os(iOS)
+        guard let scene = UIApplication.shared.connectedScenes.first as? UIWindowScene else {
+            return
+        }
+        guard let window = scene.keyWindow else {
+            return
+        }
+        switch systemAppearance {
+        case .light:
+            window.overrideUserInterfaceStyle = .light
+        case .dark:
+            window.overrideUserInterfaceStyle = .dark
+        case .none:
+            window.overrideUserInterfaceStyle = .unspecified
+        }
+#elseif os(macOS)
+        guard let app = NSApp else {
+//            assertionFailure("NSApp is being used too early")
+            return
+        }
+        switch systemAppearance {
+        case .light:
+            app.appearance = NSAppearance(named: .vibrantLight)
+        case .dark:
+            app.appearance = NSAppearance(named: .vibrantDark)
+        case .none:
+            app.appearance = nil
+        }
+#endif
     }
 }

app-apple/Sources/AppLibraryMain/Views/Providers/ProviderServerView.swift
@@ -46,7 +46,7 @@ struct ProviderServerView: View {
 
     @StateObject
     private var filtersViewModel = ProviderFiltersView.Model(
-        kvManager: KeyValueManager(store: UserDefaultsStore(.standard))
+        userPreferences: UserPreferencesObservable(kvStore: UserDefaultsStore(.standard))
     )
 
     @State

app-shared/Sources/CommonLibraryApple/UserDefaultsStore.swift
@@ -2,26 +2,29 @@
 //
 // SPDX-License-Identifier: GPL-3.0
 
-public final class UserDefaultsStore: KeyValueStore {
+public final class UserDefaultsStore: KeyValueStore, @unchecked Sendable {
     private let defaults: UserDefaults
+
+    public let fallback: [String: Any]
 
     public init(_ defaults: UserDefaults) {
         self.defaults = defaults
+        fallback = [:]
     }
 
-    public func value<V>(for key: String) -> V? {
+    public func strictObject<V>(forKey key: String) -> V? {
         defaults.object(forKey: key) as? V
     }
 
-    public func set<V>(_ value: V?, for key: String) {
-        guard let value else {
+    public func set<V>(_ object: V?, forKey key: String) {
+        guard let object else {
             defaults.removeObject(forKey: key)
             return
         }
-        defaults.set(value, forKey: key)
+        defaults.set(object, forKey: key)
     }
 
-    public func removeValue(for key: String) {
+    public func removeObject(forKey key: String) {
         defaults.removeObject(forKey: key)
     }
 }

app-shared/Sources/CommonLibraryCore/Domain/AppPreference.swift
@@ -4,14 +4,13 @@
 
 extension ABI {
     public enum AppPreference: String, PreferenceProtocol {
-
         // Not directly accessible
         case deviceId
         case configFlags
 
         // Manual
         case dnsFallsBack
-        //    case dnsFallbackServers
+//        case dnsFallbackServers
         case lastCheckedVersionDate
         case lastCheckedVersion
         case lastUsedProfileId
@@ -26,23 +25,10 @@ extension ABI {
             "App.\(rawValue)"
         }
     }
-}
 
-// WARNING: Field types must be scalar to fit UserDefaults
-extension ABI {
+    // WARNING: Field types must be scalar to fit UserDefaults
     public struct AppPreferenceValues: Hashable, Codable, Sendable {
-
-        // Override config flags only if non-nil
-        public struct Experimental: Hashable, Codable, Sendable {
-            public var ignoredConfigFlags: Set<ConfigFlag> = []
-        }
-
         public var deviceId: String?
-        // XXX: These are copied from ConfigManager.activeFlags for use
-        // in the PacketTunnelProvider (see AppContext.onApplicationActive).
-        // In the app, use ConfigManager.activeFlags directly.
-        public var configFlagsData: Data?
-
         public var dnsFallsBack = true
         public var lastCheckedVersionDate: TimeInterval?
         public var lastCheckedVersion: String?
@@ -51,14 +37,24 @@ extension ABI {
         public var relaxedVerification = false
         public var skipsPurchases = false
 
+        // XXX: These are copied from ConfigManager.activeFlags for use
+        // in the PacketTunnelProvider (see AppABI.onApplicationActive).
+        // In the app, use ConfigManager.activeFlags directly.
+        public var configFlagsData: Data?
+
+        // Encoded of type Experimental
         public var experimentalData: Data?
 
-        public init() {
-        }
+        public init() {}
     }
 }
 
 extension ABI.AppPreferenceValues {
+    public struct Experimental: Hashable, Codable, Sendable {
+        public var ignoredConfigFlags: Set<ABI.ConfigFlag> = []
+        public init() {}
+    }
+
     public var configFlags: Set<ABI.ConfigFlag> {
         get {
             guard let configFlagsData else { return [] }
@@ -77,9 +73,7 @@ extension ABI.AppPreferenceValues {
             }
         }
     }
-}
 
-extension ABI.AppPreferenceValues {
     public var experimental: Experimental {
         get {
             guard let experimentalData else { return Experimental() }
@@ -105,5 +99,37 @@ extension ABI.AppPreferenceValues {
 
     public func enabledFlags(of flags: Set<ABI.ConfigFlag>) -> Set<ABI.ConfigFlag> {
         flags.subtracting(experimental.ignoredConfigFlags)
+    }
+}
+
+extension KeyValueStore {
+    // TODO: #1513, refactor to keep automatically in sync with AppPreference
+    public var preferences: ABI.AppPreferenceValues {
+        get {
+            var values = ABI.AppPreferenceValues()
+            values.deviceId = string(forAppPreference: .deviceId)
+            values.dnsFallsBack = bool(forAppPreference: .dnsFallsBack)
+            values.lastCheckedVersionDate = double(forAppPreference: .lastCheckedVersionDate)
+            values.lastCheckedVersion = object(forAppPreference: .lastCheckedVersion)
+            values.lastUsedProfileId = object(forAppPreference: .lastUsedProfileId)
+            values.logsPrivateData = bool(forAppPreference: .logsPrivateData)
+            values.relaxedVerification = bool(forAppPreference: .relaxedVerification)
+            values.skipsPurchases = bool(forAppPreference: .skipsPurchases)
+            values.configFlagsData = object(forAppPreference: .configFlags)
+            values.experimentalData = object(forAppPreference: .experimental)
+            return values
+        }
+        set {
+            set(newValue.deviceId, forAppPreference: .dnsFallsBack)
+            set(newValue.dnsFallsBack, forAppPreference: .dnsFallsBack)
+            set(newValue.lastCheckedVersionDate, forAppPreference: .lastCheckedVersionDate)
+            set(newValue.lastCheckedVersion, forAppPreference: .lastCheckedVersion)
+            set(newValue.lastUsedProfileId, forAppPreference: .lastUsedProfileId)
+            set(newValue.logsPrivateData, forAppPreference: .logsPrivateData)
+            set(newValue.relaxedVerification, forAppPreference: .relaxedVerification)
+            set(newValue.skipsPurchases, forAppPreference: .skipsPurchases)
+            set(newValue.configFlagsData, forAppPreference: .configFlags)
+            set(newValue.experimentalData, forAppPreference: .experimental)
+        }
     }
 }

app-shared/Sources/CommonLibraryCore/Extensions/KeyValueStore+AppPreference.swift
@@ -1,0 +1,29 @@
+// SPDX-FileCopyrightText: 2025 Davide De Rosa
+//
+// SPDX-License-Identifier: GPL-3.0
+
+extension KeyValueStore {
+    public func set<V>(_ object: V?, forAppPreference pref: ABI.AppPreference) {
+        set(object, forKey: pref.key)
+    }
+
+    public func object<V>(forAppPreference pref: ABI.AppPreference) -> V? {
+        object(forKey: pref.key)
+    }
+
+    public func bool(forAppPreference pref: ABI.AppPreference) -> Bool {
+        bool(forKey: pref.key)
+    }
+
+    public func integer(forAppPreference pref: ABI.AppPreference) -> Int {
+        integer(forKey: pref.key)
+    }
+
+    public func double(forAppPreference pref: ABI.AppPreference) -> Double {
+        double(forKey: pref.key)
+    }
+
+    public func string(forAppPreference pref: ABI.AppPreference) -> String? {
+        string(forKey: pref.key)
+    }
+}

app-shared/Sources/CommonLibraryCore/Strategy/InMemoryStore.swift
@@ -1,0 +1,31 @@
+// SPDX-FileCopyrightText: 2025 Davide De Rosa
+//
+// SPDX-License-Identifier: GPL-3.0
+
+// FIXME: #1594, This is not Sendable, but only used in tests/previews
+public final class InMemoryStore: KeyValueStore, @unchecked Sendable {
+    private var map: [String: Any]
+
+    public let fallback: [String: Any]
+
+    public init(fallback: [String: Any] = [:]) {
+        map = [:]
+        self.fallback = fallback
+    }
+
+    public func contains(_ key: String) -> Bool {
+        map[key] != nil
+    }
+
+    public func strictObject<V>(forKey key: String) -> V? {
+        map[key] as? V
+    }
+
+    public func set<V>(_ object: V?, forKey key: String) {
+        map[key] = object
+    }
+
+    public func removeObject(forKey key: String) {
+        map.removeValue(forKey: key)
+    }
+}

app-shared/Sources/CommonLibraryCore/Utils/KeyValueStore.swift
@@ -2,10 +2,61 @@
 //
 // SPDX-License-Identifier: GPL-3.0
 
-public protocol KeyValueStore {
-    func value<V>(for key: String) -> V?
+public protocol KeyValueStore: AnyObject, Sendable {
+    var fallback: [String: Any] { get }
 
-    mutating func set<V>(_ value: V?, for key: String)
+    func strictObject<V>(forKey key: String) -> V?
 
-    mutating func removeValue(for key: String)
+    func object<V>(forKey key: String) -> V?
+
+    func set<V>(_ object: V?, forKey key: String)
+
+    func removeObject(forKey key: String)
+}
+
+extension KeyValueStore {
+    public subscript<V>(_ key: String) -> V? {
+        get {
+            object(forKey: key)
+        }
+        set {
+            set(newValue, forKey: key)
+        }
+    }
+
+    public func object<V>(forKey key: String) -> V? {
+        strictObject(forKey: key) ?? fallback[key] as? V
+    }
+
+    public func bool(forKey key: String) -> Bool {
+        var value = self[key] as Bool?
+        if value == nil {
+            value = fallback[key] as? Bool
+        }
+        return value ?? false
+    }
+
+    public func integer(forKey key: String) -> Int {
+        var value = self[key] as Int?
+        if value == nil {
+            value = fallback[key] as? Int
+        }
+        return value ?? 0
+    }
+
+    public func double(forKey key: String) -> Double {
+        var value = self[key] as Double?
+        if value == nil {
+            value = fallback[key] as? Double
+        }
+        return value ?? 0.0
+    }
+
+    public func string(forKey key: String) -> String? {
+        var value = self[key] as String?
+        if value == nil {
+            value = fallback[key] as? String
+        }
+        return value
+    }
 }

commit 8cc6a1efbff08033b567a9666bcf611d0b0c5af6

Fix regression in some default preferences (#1651)
Evident on clean installs. These must be:

- dnsFallsBack = true
- pinsActiveProfile = true

Regression in #1640.

Precisely, this bit fell into the cracks:

```swift
public convenience init(store: KeyValueStore, fallback: ABI.AppPreferenceValues) {
        let values = [
            ABI.AppPreference.dnsFallsBack.key: fallback.dnsFallsBack,
            ABI.AppPreference.logsPrivateData.key: fallback.logsPrivateData
        ]
        self.init(store: store, fallback: values)
    }
```

On the other hand, .pinsActiveProfile was true by default via SwiftUI
AppStorage.

app-apple/Sources/AppLibrary/Extensions/KeyValueStore+UIPreference.swift
@@ -13,19 +13,19 @@ extension KeyValueStore {
         object(forKey: pref.key)
     }
 
-    func bool(forUIPreference pref: UIPreference) -> Bool {
-        bool(forKey: pref.key)
-    }
-
-    func integer(forUIPreference pref: UIPreference) -> Int {
-        integer(forKey: pref.key)
-    }
-
-    func double(forUIPreference pref: UIPreference) -> Double {
-        double(forKey: pref.key)
-    }
-
     func string(forUIPreference pref: UIPreference) -> String? {
         string(forKey: pref.key)
+    }
+
+    func bool(forUIPreference pref: UIPreference, fallback: Bool = false) -> Bool {
+        bool(forKey: pref.key) ?? fallback
+    }
+
+    func integer(forUIPreference pref: UIPreference, fallback: Int = 0) -> Int {
+        integer(forKey: pref.key) ?? fallback
+    }
+
+    func double(forUIPreference pref: UIPreference, fallback: Double = 0.0) -> Double {
+        double(forKey: pref.key) ?? fallback
     }
 }

app-apple/Sources/AppLibrary/Observables/UserPreferencesObservable.swift
@@ -15,7 +15,7 @@ public final class UserPreferencesObservable {
         self.logger = logger
         self.kvStore = kvStore
 
-        dnsFallsBack = kvStore.bool(forAppPreference: .dnsFallsBack)
+        dnsFallsBack = kvStore.bool(forAppPreference: .dnsFallsBack, fallback: true)
         if let experimentalData = kvStore.object(forAppPreference: .experimental) as Data? {
             do {
                 experimental = try JSONDecoder().decode(ABI.AppPreferenceValues.Experimental.self, from: experimentalData)
@@ -34,7 +34,7 @@ public final class UserPreferencesObservable {
             OnboardingStep(rawValue: $0)
         }
         onlyShowsFavorites = kvStore.bool(forUIPreference: .onlyShowsFavorites)
-        pinsActiveProfile = kvStore.bool(forUIPreference: .pinsActiveProfile)
+        pinsActiveProfile = kvStore.bool(forUIPreference: .pinsActiveProfile, fallback: true)
         profilesLayout = kvStore.string(forUIPreference: .profilesLayout).flatMap {
             ProfilesLayout(rawValue: $0)
         } ?? .list

app-apple/Sources/AppLibraryMain/Views/Providers/ProviderServerView.swift
@@ -46,7 +46,10 @@ struct ProviderServerView: View {
 
     @StateObject
     private var filtersViewModel = ProviderFiltersView.Model(
-        userPreferences: UserPreferencesObservable(logger: PartoutAppLogger(), kvStore: UserDefaultsStore(.standard))
+        userPreferences: UserPreferencesObservable(
+            logger: PartoutAppLogger(),
+            kvStore: UserDefaultsStore(.standard)
+        )
     )
 
     @State

app-shared/Sources/CommonLibraryApple/UserDefaultsStore.swift
@@ -5,14 +5,11 @@
 public final class UserDefaultsStore: KeyValueStore, @unchecked Sendable {
     private let defaults: UserDefaults
 
-    public let fallback: [String: Any]
-
     public init(_ defaults: UserDefaults) {
         self.defaults = defaults
-        fallback = [:]
     }
 
-    public func strictObject<V>(forKey key: String) -> V? {
+    public func object<V>(forKey key: String) -> V? {
         defaults.object(forKey: key) as? V
     }
 

app-shared/Sources/CommonLibraryCore/Domain/AppPreference.swift
@@ -108,7 +108,7 @@ extension KeyValueStore {
         get {
             var values = ABI.AppPreferenceValues()
             values.deviceId = string(forAppPreference: .deviceId)
-            values.dnsFallsBack = bool(forAppPreference: .dnsFallsBack)
+            values.dnsFallsBack = bool(forAppPreference: .dnsFallsBack, fallback: true)
             values.lastCheckedVersionDate = double(forAppPreference: .lastCheckedVersionDate)
             values.lastCheckedVersion = object(forAppPreference: .lastCheckedVersion)
             values.lastUsedProfileId = object(forAppPreference: .lastUsedProfileId)

app-shared/Sources/CommonLibraryCore/Extensions/KeyValueStore+AppPreference.swift
@@ -11,19 +11,19 @@ extension KeyValueStore {
         object(forKey: pref.key)
     }
 
-    public func bool(forAppPreference pref: ABI.AppPreference) -> Bool {
-        bool(forKey: pref.key)
-    }
-
-    public func integer(forAppPreference pref: ABI.AppPreference) -> Int {
-        integer(forKey: pref.key)
-    }
-
-    public func double(forAppPreference pref: ABI.AppPreference) -> Double {
-        double(forKey: pref.key)
-    }
-
     public func string(forAppPreference pref: ABI.AppPreference) -> String? {
         string(forKey: pref.key)
+    }
+
+    public func bool(forAppPreference pref: ABI.AppPreference, fallback: Bool = false) -> Bool {
+        bool(forKey: pref.key) ?? fallback
+    }
+
+    public func integer(forAppPreference pref: ABI.AppPreference, fallback: Int = 0) -> Int {
+        integer(forKey: pref.key) ?? fallback
+    }
+
+    public func double(forAppPreference pref: ABI.AppPreference, fallback: Double = 0.0) -> Double {
+        double(forKey: pref.key) ?? fallback
     }
 }

app-shared/Sources/CommonLibraryCore/Strategy/InMemoryStore.swift
@@ -6,18 +6,15 @@
 public final class InMemoryStore: KeyValueStore, @unchecked Sendable {
     private var map: [String: Any]
 
-    public let fallback: [String: Any]
-
-    public init(fallback: [String: Any] = [:]) {
+    public init() {
         map = [:]
-        self.fallback = fallback
     }
 
     public func contains(_ key: String) -> Bool {
         map[key] != nil
     }
 
-    public func strictObject<V>(forKey key: String) -> V? {
+    public func object<V>(forKey key: String) -> V? {
         map[key] as? V
     }
 

app-shared/Sources/CommonLibraryCore/Utils/KeyValueStore.swift
@@ -3,10 +3,6 @@
 // SPDX-License-Identifier: GPL-3.0
 
 public protocol KeyValueStore: AnyObject, Sendable {
-    var fallback: [String: Any] { get }
-
-    func strictObject<V>(forKey key: String) -> V?
-
     func object<V>(forKey key: String) -> V?
 
     func set<V>(_ object: V?, forKey key: String)
@@ -24,39 +20,19 @@ extension KeyValueStore {
         }
     }
 
-    public func object<V>(forKey key: String) -> V? {
-        strictObject(forKey: key) ?? fallback[key] as? V
+    public func bool(forKey key: String) -> Bool? {
+        self[key] as Bool?
     }
 
-    public func bool(forKey key: String) -> Bool {
-        var value = self[key] as Bool?
-        if value == nil {
-            value = fallback[key] as? Bool
-        }
-        return value ?? false
+    public func integer(forKey key: String) -> Int? {
+        self[key] as Int?
     }
 
-    public func integer(forKey key: String) -> Int {
-        var value = self[key] as Int?
-        if value == nil {
-            value = fallback[key] as? Int
-        }
-        return value ?? 0
-    }
-
-    public func double(forKey key: String) -> Double {
-        var value = self[key] as Double?
-        if value == nil {
-            value = fallback[key] as? Double
-        }
-        return value ?? 0.0
+    public func double(forKey key: String) -> Double? {
+        self[key] as Double?
     }
 
     public func string(forKey key: String) -> String? {
-        var value = self[key] as String?
-        if value == nil {
-            value = fallback[key] as? String
-        }
-        return value
+        self[key] as String?
     }
 }

app-shared/Tests/CommonLibraryTests/Business/KeyValueStoreTests.swift
@@ -30,21 +30,6 @@ struct KeyValueStoreTests {
     }
 
     @Test
-    func givenKeyValue_whenSetFallback_thenGetsFallback() {
-        let sut = InMemoryStore(fallback: [
-            "string": "foobar",
-            "boolean": true,
-            "number": 123
-        ])
-        #expect(sut.object(forKey: "string") == "foobar")
-        #expect(sut.object(forKey: "boolean") == true)
-        #expect(sut.object(forKey: "number") == 123)
-        #expect(sut.string(forKey: "string") == "foobar")
-        #expect(sut.bool(forKey: "boolean") == true)
-        #expect(sut.integer(forKey: "number") == 123)
-    }
-
-    @Test
     func givenKeyValue_whenSetConfigFlags_thenIsExpected() throws {
         let flags: Set<ABI.ConfigFlag> = [.neSocketUDP, .allowsRelaxedVerification]
         let sut = InMemoryStore()

app-shared/Tests/CommonLibraryTests/Domain/AppPreferenceTests.swift
@@ -19,6 +19,22 @@ struct AppPreferenceTests {
         #expect(experimental == sut.experimental)
     }
 
+    // Value types don't matter to the test
+    @Test
+    func givenKeyValue_whenSetFallback_thenGetsFallback() {
+        let sut = InMemoryStore()
+        #expect(sut.bool(forAppPreference: .dnsFallsBack, fallback: true))
+        #expect(sut.integer(forAppPreference: .deviceId, fallback: 100) == 100)
+        #expect(sut.double(forAppPreference: .lastUsedProfileId, fallback: 200.55) == 200.55)
+
+        sut.set(false, forAppPreference: .dnsFallsBack)
+        sut.set(500, forAppPreference: .deviceId)
+        sut.set(800.88, forAppPreference: .lastUsedProfileId)
+        #expect(!sut.bool(forAppPreference: .dnsFallsBack, fallback: true))
+        #expect(sut.integer(forAppPreference: .deviceId, fallback: 100) == 500)
+        #expect(sut.double(forAppPreference: .lastUsedProfileId, fallback: 200.55) == 800.88)
+    }
+
     @Test
     func givenExperimental_whenIgnoreFlags_thenIsApplied() {
         var sut = ABI.AppPreferenceValues()