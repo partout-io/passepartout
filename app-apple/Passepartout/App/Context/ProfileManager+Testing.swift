9590a7c1117ddafb634d937702d921364cedb223
Davide <keeshux@gmail.com>
2026-01-15 14:28:55 +0100

    Clean up

--- app-apple/Passepartout/App/Context/ProfileManager+Testing.swift
+++ app-apple/Passepartout/App/Context/ProfileManager+Testing.swift
@@ -2,8 +2,8 @@
 //
 // SPDX-License-Identifier: GPL-3.0
 
+import AppLibrary
 import CommonLibrary
-import Foundation
 
 extension ProfileManager {
     public static func forUITesting(withRegistry registry: Registry, processor: ProfileProcessor) -> ProfileManager {
@@ -20,7 +20,7 @@ extension ProfileManager {
                     var builder = Profile.Builder()
                     builder.name = parameters.name
                     builder.attributes.isAvailableForTV = parameters.isTV
-                    var onDemandIdIfDisabled: UUID?
+                    var onDemandIdIfDisabled: UniqueID?
 
                     for moduleType in parameters.moduleTypes {
                         var moduleBuilder = moduleType.newModule(with: registry)
