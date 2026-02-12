partout-io/passepartout

commit 207df7fde7ea3f03816e6803e15f8b4f64a5c437

Clean up deprecated code (#1682)
- Live with Partout imports
- Delete deprecated code
- Make managers private in favor of observables (except API/Preferences)
- Drop ABI.AppProfile wrapper of Profile
- Drop ABI.AppIdentifier alias of Profile.ID
- Rename ABI.AppProfile.Status/Info to ABI.AppProfileStatus/Info
- Get isRemotelyShared from latest profile headers (sharing flags)
- Fix deprecated .onChange (pick 2nd arg)
- Fix retroactive conformances
- Make more entities internal
- Repurpose FIXMEs to move on

app-apple/Passepartout/Tunnel/PacketTunnelProvider.swift
@@ -5,8 +5,6 @@
 import AppResources
 import CommonLibrary
 @preconcurrency import NetworkExtension
-// FIXME: #1594, Drop import
-import Partout
 
 final class PacketTunnelProvider: NEPacketTunnelProvider, @unchecked Sendable {
     private var abi: TunnelABIProtocol?
@@ -53,14 +51,12 @@ final class PacketTunnelProvider: NEPacketTunnelProvider, @unchecked Sendable {
         }()
 
         // Update or fetch existing preferences
-        let (kvStore, preferences) = {
+        let preferences = {
             let kvStore = appConfiguration.newKeyValueStore()
             if let startPreferences {
                 kvStore.preferences = startPreferences
-                return (kvStore, startPreferences)
-            } else {
-                return (kvStore, kvStore.preferences)
             }
+            return kvStore.preferences
         }()
 
         // Defer to ABI

app-shared/Sources/CommonLibraryCore/Domain/AppPreference.swift
@@ -2,7 +2,6 @@
 //
 // SPDX-License-Identifier: GPL-3.0
 
-// FIXME: #1594, Drop import (Profile.ID -> UniqueID)
 import Partout
 
 extension ABI {

commit 40192bd3f95cea559be7dd23786701a85e13b7b0

Fix assertion failure on nil .deviceId in tunnel start (#1691)
The tunnel was literally unable to start while debugging because of an
early assertion failure about the missing `deviceId` preference.

The regression comes from #1682 where PacketTunnelProvider was
refactored to return `kvStore.preferences` even when `startPreferences`
was set.

This code:

```swift
kvStore.preferences = startPreferences
return startPreferences
```

Was rewritten to:

```swift
kvStore.preferences = startPreferences
return kvStore.preferences
```

But this was not equivalent because the `.deviceId` field was not copied
due to a typo in AppPreferenceValues mapping. This PR adds a convenient
assertion to guarantee the equivalence:

```swift
kvStore.preferences = startPreferences
assert(kvStore.preferences == startPreferences)
```

app-apple/Passepartout/Tunnel/PacketTunnelProvider.swift
@@ -55,6 +55,10 @@ final class PacketTunnelProvider: NEPacketTunnelProvider, @unchecked Sendable {
             let kvStore = appConfiguration.newKeyValueStore()
             if let startPreferences {
                 kvStore.preferences = startPreferences
+                pspLog(.core, .debug, "PTP: kvStore.preferences: \(kvStore.preferences)")
+                pspLog(.core, .debug, "PTP: startPreferences: \(startPreferences)")
+                assert(kvStore.preferences == startPreferences)
+                return startPreferences
             }
             return kvStore.preferences
         }()

app-shared/Sources/CommonLibraryCore/Domain/AppPreference.swift
@@ -124,7 +124,7 @@ extension KeyValueStore {
             return values
         }
         set {
-            set(newValue.deviceId, forAppPreference: .dnsFallsBack)
+            set(newValue.deviceId, forAppPreference: .deviceId)
             set(newValue.dnsFallsBack, forAppPreference: .dnsFallsBack)
             set(newValue.lastCheckedVersionDate, forAppPreference: .lastCheckedVersionDate)
             set(newValue.lastCheckedVersion, forAppPreference: .lastCheckedVersion)