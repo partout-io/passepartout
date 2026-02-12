partout-io/passepartout

commit f17936b74f8d62414ddefb755dd890342a1523d4

Fix views not reacting to changes to observables (#1687)
Getters must refer to local observable properties, not ABI methods.
Therefore, fetch ABI events into observables' local state, rather than
pulling state from ABI, which doesn't cause view redraws.

E.g. For views to redraw on changes to in-app eligibility,
IAPObservable.isEligible(for:) must use local .eligibleFeatures rather
than querying abi.isEligible(for:)

- ConfigObservable
  - Attach JSON data to `.refresh` event
- IAPObservable
- Get `originalPurchase`, `purchasedProducts`, and `isBeta` from
`.newReceipt` event
  - Infer eligibility from local `eligibleFeatures`
- ProfileObservable
- Rely on `.changeRemoteImporting` to set initial value for
isRemoteImportingEnabled
- VersionObservable
  - Store latest release on `.new` event

app-shared/Sources/CommonLibraryCore/Business/ConfigManager.swift
@@ -20,7 +20,8 @@ public final class ConfigManager {
                 synchronousActiveFlags = Set(ABI.ConfigFlag.allCases.filter {
                     isActive($0)
                 })
-                didChange.send(.refresh(synchronousActiveFlags))
+                let data = bundle?.map.compactMapValues(\.data) ?? [:]
+                didChange.send(.refresh(synchronousActiveFlags, data: data))
             }
         }
     }

app-apple/Sources/AppLibrary/Observables/AppContext.swift
@@ -39,7 +39,7 @@ public final class AppContext {
 
         // ABI
         appEncoderObservable = AppEncoderObservable(abi: abi.encoder)
-        configObservable = ConfigObservable(abi: abi.config)
+        configObservable = ConfigObservable()
         iapObservable = IAPObservable(abi: abi.iap)
         profileObservable = ProfileObservable(abi: abi.profile)
         registryObservable = RegistryObservable(abi: abi.registry)

app-apple/Sources/AppLibrary/Observables/ConfigObservable.swift
@@ -8,13 +8,12 @@ import Observation
 
 @MainActor @Observable
 public final class ConfigObservable {
-    private let abi: AppABIConfigProtocol
-
     public private(set) var activeFlags: Set<ABI.ConfigFlag>
+    public private(set) var allData: [ABI.ConfigFlag: JSON]
 
-    public init(abi: AppABIConfigProtocol) {
-        self.abi = abi
+    public init() {
         activeFlags = []
+        allData = [:]
     }
 
     public func isActive(_ flag: ABI.ConfigFlag) -> Bool {
@@ -22,14 +21,15 @@ public final class ConfigObservable {
     }
 
     public func data(for flag: ABI.ConfigFlag) -> JSON? {
-        abi.data(for: flag)
+        allData[flag]
     }
 
     func onUpdate(_ event: ABI.ConfigEvent) {
         pspLog(.core, .debug, "ConfigObservable.onUpdate(): \(event)")
         switch event {
-        case .refresh(let flags):
+        case .refresh(let flags, let data):
             activeFlags = flags
+            allData = data
         }
     }
 }

app-apple/Sources/AppLibrary/Observables/IAPObservable.swift
@@ -10,16 +10,24 @@ public final class IAPObservable {
     private let abi: AppABIIAPProtocol
 
     public private(set) var isEnabled: Bool
-    public private(set) var originalPurchase: ABI.OriginalPurchase?
-    public private(set) var eligibleFeatures: Set<ABI.AppFeature>
     public private(set) var isLoadingReceipt: Bool
+    public private(set) var isBeta: Bool
+    public private(set) var originalPurchase: ABI.OriginalPurchase?
+    public private(set) var purchasedProducts: Set<ABI.AppProduct>
+    public private(set) var eligibleFeatures: Set<ABI.AppFeature>
+    public private(set) var isEligibleForComplete: Bool
+    public private(set) var isEligibleForFeedback: Bool
     private var subscription: Task<Void, Never>?
 
     public init(abi: AppABIIAPProtocol) {
         self.abi = abi
-        isEnabled = abi.isEnabled
+        isEnabled = true
+        isLoadingReceipt = true
+        isBeta = false
+        purchasedProducts = []
         eligibleFeatures = []
-        isLoadingReceipt = false
+        isEligibleForComplete = false
+        isEligibleForFeedback = false
     }
 }
 
@@ -61,36 +69,16 @@ extension IAPObservable {
         try await abi.purchasableProducts(for: products)
     }
 
-    public var purchasedProducts: Set<ABI.AppProduct> {
-        abi.purchasedProducts
-    }
-
-    public var isBeta: Bool {
-        abi.isBeta
+    public var verificationDelayMinutes: Int {
+        abi.verificationDelayMinutes
     }
 
     public func isEligible(for feature: ABI.AppFeature) -> Bool {
-        abi.isEligible(for: feature)
+        eligibleFeatures.contains(feature)
     }
 
-    public func isEligible(for features: Set<ABI.AppFeature>) -> Bool {
-        abi.isEligible(for: features)
-    }
-
-    public var isEligibleForFeedback: Bool {
-        abi.isEligibleForFeedback
-    }
-
-    public var isEligibleForComplete: Bool {
-        abi.isEligibleForComplete
-    }
-
-    public var isPayingUser: Bool {
-        !purchasedProducts.isEmpty
-    }
-
-    public var verificationDelayMinutes: Int {
-        abi.verificationDelayMinutes
+    public func isEligible<C>(for features: C) -> Bool where C: Collection, C.Element == ABI.AppFeature {
+        features.isEmpty || features.allSatisfy(eligibleFeatures.contains)
     }
 
     public var didPurchaseComplete: Bool {
@@ -109,13 +97,16 @@ extension IAPObservable {
         switch event {
         case .status(let isEnabled):
             self.isEnabled = isEnabled
-        case .eligibleFeatures(let features):
-            eligibleFeatures = features
         case .loadReceipt(let isLoading):
             isLoadingReceipt = isLoading
-            if !isLoading {
-                originalPurchase = abi.originalPurchase
-            }
+        case .newReceipt(let purchase, let products, let isBeta):
+            originalPurchase = purchase
+            purchasedProducts = products
+            self.isBeta = isBeta
+        case .eligibleFeatures(let features, let forComplete, let forFeedback):
+            eligibleFeatures = features
+            isEligibleForComplete = forComplete
+            isEligibleForFeedback = forFeedback
         }
     }
 }

app-apple/Sources/AppLibrary/Observables/ProfileObservable.swift
@@ -27,7 +27,7 @@ public final class ProfileObservable {
         allHeaders = [:]
         filteredHeaders = []
         isReady = false
-        isRemoteImportingEnabled = abi.isRemoteImportingEnabled
+        isRemoteImportingEnabled = false
         searchSubject = CurrentValueSubject("")
 
         observeEvents(searchDebounce: searchDebounce)

app-apple/Sources/AppLibrary/Observables/VersionObservable.swift
@@ -21,8 +21,8 @@ public final class VersionObservable {
 
     func onUpdate(_ event: ABI.VersionEvent) {
         switch event {
-        case .new:
-            latestRelease = abi.latestRelease
+        case .new(let release):
+            latestRelease = release
         }
     }
 }

app-shared/Sources/CommonLibraryCore/ABI/AppABI.swift
@@ -7,7 +7,6 @@ import Partout
 @MainActor
 public final class AppABI: Sendable {
     // Public ABI by domain
-    public let config: AppABIConfigProtocol
     public let encoder: AppABIEncoderProtocol
     public let iap: AppABIIAPProtocol
     public let profile: AppABIProfileProtocol
@@ -77,7 +76,6 @@ public final class AppABI: Sendable {
 
         iapManager.isEnabled = appConfiguration.distributionTarget.supportsIAP && !kvStore.bool(forAppPreference: .skipsPurchases)
 
-        config = AppABIConfig(configManager: configManager)
         encoder = AppABIEncoder(appEncoder: appEncoder)
         iap = AppABIIAP(
             iapManager: iapManager,
@@ -169,18 +167,6 @@ extension AppABI: AppABILoggerProtocol, LogFormatter {
 
 // MARK: - Actions
 
-private struct AppABIConfig: AppABIConfigProtocol {
-    let configManager: ConfigManager
-
-    var activeFlags: Set<ABI.ConfigFlag> {
-        configManager.activeFlags
-    }
-
-    func data(for flag: ABI.ConfigFlag) -> JSON? {
-        configManager.data(for: flag)
-    }
-}
-
 private struct AppABIEncoder: AppABIEncoderProtocol {
     let appEncoder: AppEncoder
 
@@ -205,10 +191,6 @@ private struct AppABIIAP: AppABIIAPProtocol {
     let iapManager: IAPManager
     let kvStore: KeyValueStore
     let supportsIAP: Bool
-
-    var isEnabled: Bool {
-        iapManager.isEnabled
-    }
 
     func enable(_ isEnabled: Bool) {
         iapManager.isEnabled = supportsIAP && isEnabled
@@ -237,34 +219,6 @@ private struct AppABIIAP: AppABIIAPProtocol {
 
     func purchasableProducts(for products: [ABI.AppProduct]) async throws -> [ABI.StoreProduct] {
         try await iapManager.fetchPurchasableProducts(for: products)
-    }
-
-    var originalPurchase: ABI.OriginalPurchase? {
-        iapManager.originalPurchase
-    }
-
-    var purchasedProducts: Set<ABI.AppProduct> {
-        iapManager.purchasedProducts
-    }
-
-    var isBeta: Bool {
-        iapManager.isBeta
-    }
-
-    func isEligible(for feature: ABI.AppFeature) -> Bool {
-        iapManager.isEligible(for: feature)
-    }
-
-    func isEligible(for features: Set<ABI.AppFeature>) -> Bool {
-        iapManager.isEligible(for: features)
-    }
-
-    var isEligibleForFeedback: Bool {
-        iapManager.isEligibleForFeedback
-    }
-
-    var isEligibleForComplete: Bool {
-        iapManager.isEligibleForComplete
     }
 
     var verificationDelayMinutes: Int {
@@ -318,10 +272,6 @@ private struct AppABIProfile: AppABIProfileProtocol {
 
     func removeAllRemote() async throws {
         try await profileManager.eraseRemotelySharedProfiles()
-    }
-
-    var isRemoteImportingEnabled: Bool {
-        profileManager.isRemoteImportingEnabled
     }
 }
 
@@ -397,10 +347,6 @@ private struct AppABIVersion: AppABIVersionProtocol {
     func checkLatestRelease() async {
         await versionChecker.checkLatestRelease()
     }
-
-    var latestRelease: ABI.VersionRelease? {
-        versionChecker.latestRelease
-    }
 }
 
 private struct AppABIWebReceiver: AppABIWebReceiverProtocol {
@@ -470,7 +416,7 @@ private extension AppABI {
                     kvStore.set(!isEnabled, forAppPreference: .skipsPurchases)
                     await iapManager.reloadReceipt()
                     didLoadReceiptDate = Date()
-                case .eligibleFeatures(let features):
+                case .eligibleFeatures(let features, _, _):
                     // XXX: This was on .dropFirst() + .removeDuplicates()
                     do {
                         pspLog(.iap, .info, "IAPManager.eligibleFeatures -> \(features)")

app-shared/Sources/CommonLibraryCore/ABI/AppABIProtocol.swift
@@ -8,12 +8,6 @@ import Partout
 // MARK: Domains
 
 @MainActor
-public protocol AppABIConfigProtocol: Sendable {
-    var activeFlags: Set<ABI.ConfigFlag> { get }
-    func data(for flag: ABI.ConfigFlag) -> JSON?
-}
-
-@MainActor
 public protocol AppABIEncoderProtocol: Sendable {
     func defaultFilename(for profileName: String) -> String
     func profile(fromString string: String) throws -> Profile
@@ -23,7 +17,6 @@ public protocol AppABIEncoderProtocol: Sendable {
 
 @MainActor
 public protocol AppABIIAPProtocol: Sendable {
-    var isEnabled: Bool { get }
     func enable(_ isEnabled: Bool)
     func purchase(_ storeProduct: ABI.StoreProduct) async throws -> ABI.StoreResult
     func verify(_ profile: Profile, extra: Set<ABI.AppFeature>?) throws
@@ -31,13 +24,6 @@ public protocol AppABIIAPProtocol: Sendable {
     func restorePurchases() async throws
     func suggestedProducts(for features: Set<ABI.AppFeature>, hints: Set<ABI.StoreProductHint>?) -> Set<ABI.AppProduct>
     func purchasableProducts(for products: [ABI.AppProduct]) async throws -> [ABI.StoreProduct]
-    var originalPurchase: ABI.OriginalPurchase? { get }
-    var purchasedProducts: Set<ABI.AppProduct> { get }
-    var isBeta: Bool { get }
-    func isEligible(for feature: ABI.AppFeature) -> Bool
-    func isEligible(for features: Set<ABI.AppFeature>) -> Bool
-    var isEligibleForFeedback: Bool { get }
-    var isEligibleForComplete: Bool { get }
     var verificationDelayMinutes: Int { get }
 }
 
@@ -58,7 +44,6 @@ public protocol AppABIProfileProtocol: Sendable {
     func remove(_ id: Profile.ID) async
     func remove(_ ids: [Profile.ID]) async
     func removeAllRemote() async throws
-    var isRemoteImportingEnabled: Bool { get }
 }
 
 @MainActor
@@ -80,6 +65,7 @@ public protocol AppABITunnelProtocol: Sendable {
 //    func reconnect(to profileId: Profile.ID) async throws
     func disconnect(from profileId: Profile.ID) async throws
     func currentLog() async -> [ABI.AppLogLine]
+    // These are non-observable (pull manually)
     func lastError(ofProfileId profileId: Profile.ID) -> ABI.AppError?
     func transfer(ofProfileId profileId: Profile.ID) -> ABI.ProfileTransfer?
     func environmentValue(for key: AppABITunnelValueKey, ofProfileId profileId: Profile.ID) -> Any?
@@ -88,7 +74,6 @@ public protocol AppABITunnelProtocol: Sendable {
 @MainActor
 public protocol AppABIVersionProtocol: Sendable {
     func checkLatestRelease() async
-    var latestRelease: ABI.VersionRelease? { get }
 }
 
 @MainActor

commit f9eeebb652b9a892c4923062155a8bb77f6bbce0

Fix regression in active ConfigFlag data
ConfigObservable was not excluding data of inactive flags.

Broken in #1687

app-shared/Sources/CommonLibraryCore/Business/ConfigManager.swift
@@ -20,7 +20,9 @@ public final class ConfigManager {
                 synchronousActiveFlags = Set(ABI.ConfigFlag.allCases.filter {
                     isActive($0)
                 })
-                let data = bundle?.map.compactMapValues(\.data) ?? [:]
+                let data = bundle?.map
+                    .filter { activeFlags.contains($0.key) }
+                    .compactMapValues(\.data) ?? [:]
                 didChange.send(.refresh(synchronousActiveFlags, data: data))
             }
         }