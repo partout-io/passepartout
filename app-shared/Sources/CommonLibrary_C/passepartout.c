a633ae84f7f561e17a115c7a3c03ee6d6c581609
Davide <keeshux@gmail.com>
2026-03-04 10:00:58 +0100

    Fix and polish psp_readfile()

--- app-shared/Sources/CommonLibrary_C/passepartout.c
+++ app-shared/Sources/CommonLibrary_C/passepartout.c
@@ -10,39 +10,46 @@
 
 char *psp_readfile(const char *rel_path, const char *parent) {
     char *abs_path = NULL;
+    FILE *file = NULL;
+    char *buffer = NULL;
+
+    /* Prepend parent if not NULL. */
     if (parent) {
-        size_t path_len = snprintf(NULL, 0, "%s/%s", parent, rel_path);
+        const int path_len = snprintf(NULL, 0, "%s/%s", parent, rel_path);
+        if (path_len < 0) goto failure;
         abs_path = calloc(1, path_len + 1);
+        if (!abs_path) goto failure;
         snprintf(abs_path, path_len + 1, "%s/%s", parent, rel_path);
     } else {
         abs_path = strdup(rel_path);
+        if (!abs_path) goto failure;
     }
-    FILE *file = fopen(abs_path, "rb");
-    free(abs_path);
 
-    if (!file) return NULL;
-    if (fseek(file, 0, SEEK_END) != 0) {
-        fclose(file);
-        return NULL;
-    }
+    /* Open file at absolute path. */
+    file = fopen(abs_path, "rb");
+    if (!file) goto failure;
+    free(abs_path);
+    abs_path = NULL;
+
+    /* Compute file size. */
+    if (fseek(file, 0, SEEK_END) != 0) goto failure;
     long size = ftell(file);
-    if (size < 0) {
-        fclose(file);
-        return NULL;
-    }
+    if (size < 0 || (unsigned long)size > SIZE_MAX - 1) goto failure;
     rewind(file);
-    // Allocate buffer (+1 for null terminator)
-    char *buffer = malloc(size + 1);
-    if (!buffer) {
-        fclose(file);
-        return NULL;
-    }
+
+    /* Allocate buffer (+1 for '\0'). */
+    buffer = malloc(size + 1);
+    if (!buffer) goto failure;
     size_t read_size = fread(buffer, 1, size, file);
     fclose(file);
-    if (read_size != (size_t)size) {
-        free(buffer);
-        return NULL;
-    }
-    buffer[size] = '\0';  // Null-terminate
+    file = NULL;
+
+    if (read_size != (size_t)size) goto failure;
+    buffer[size] = '\0';
     return buffer;
+failure:
+    if (buffer) free(buffer);
+    if (file) fclose(file);
+    if (abs_path) free(abs_path);
+    return NULL;
 }
