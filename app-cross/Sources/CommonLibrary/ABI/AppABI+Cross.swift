bf5ad56c5160c9adf17ba8cf3d5b5056061df4e7
Davide <keeshux@gmail.com>
2026-03-08 15:59:06 +0100

    Add cross-platform ABI initializer

---
+++ app-cross/Sources/CommonLibrary/ABI/AppABI+Cross.swift
@@ -0,0 +1,111 @@
+// SPDX-FileCopyrightText: 2026 Davide De Rosa
+//
+// SPDX-License-Identifier: GPL-3.0
+
+#if PSP_ABI
+import CommonLibrary_C
+import Partout
+
+extension AppABI {
+    public static func forCrossPlatform(
+        appBundleData: Data,
+        appConstantsData: Data,
+        preferencesData: Data?,
+        profilesDir: String,
+        cachesURL: URL,
+        eventContext: UnsafeMutableRawPointer?,
+        eventCallback: psp_event_callback?
+    ) throws -> AppABI {
+        let decoder = JSONDecoder()
+
+        // Parse preferences
+        let preferences: ABI.AppPreferenceValues = {
+            var values = ABI.AppPreferenceValues()
+            if let preferencesData {
+                do {
+                    values = try decoder.decode(ABI.AppPreferenceValues.self, from: preferencesData)
+                } catch {
+                    pspLog(.core, .error, "Unable to decode preferences: \(error)")
+                }
+            } else {
+                pspLog(.core, .info, "No preferences provided")
+            }
+            // FIXME: #1656, C ABI, app device ID
+            if values.deviceId == nil {
+                values.deviceId = ""
+            }
+            return values
+        }()
+
+        // Decode app configuration
+        let bundle = try decoder.decode(ABI.AppBundle.self, from: appBundleData)
+        let constants = try decoder.decode(ABI.AppConstants.self, from: appConstantsData)
+        let appConfiguration = ABI.AppConfiguration(bundle: bundle, constants: constants)
+
+        // FIXME: #1656, C ABI, get cross-platform from AppConfiguration
+
+        // FIXME: #1656, C ABI, cross-platform log formatter and preferences
+        let logFormatter = DummyLogFormatter()
+        let kvStore = InMemoryStore()
+
+        // Logging context
+        let ctx = pspLogRegister(
+            for: .app,
+            with: appConfiguration,
+            preferences: preferences,
+            mapper: {
+                logFormatter.formattedLog(timestamp: $0.timestamp, message: $0.message)
+            }
+        )
+
+        // Initialize objects from global configuration
+        let configManager = appConfiguration.newConfigManager(
+            withTestBundle: false,
+            isBeta: {
+                false
+            },
+            fetcher: { _ in
+                // FIXME: #1656, C ABI, cross-platform URL requests
+                Data()
+            }
+        )
+        let registry = appConfiguration.newAppRegistry(
+            configManager: configManager,
+            kvStore: kvStore,
+            cachesURL: cachesURL
+        )
+
+        let appEncoder = AppEncoder(registry: registry)
+        // FIXME: #1656, C ABI, profile manager (real profiles)
+        let profileManager = ProfileManager(profiles: [])
+        // FIXME: #1656, C ABI, tunnel manager (real tunnel)
+        let tunnel = Tunnel(ctx, strategy: FakeTunnelStrategy()) { @Sendable profileId in
+            // FIXME: #1656, C ABI, IPC tunnel environment
+            SharedTunnelEnvironment(profileId: profileId)
+        }
+        let tunnelManager = TunnelManager(tunnel: tunnel, interval: 1.0)
+
+        // Dummy
+        let iapManager = IAPManager()
+        let versionChecker = VersionChecker()
+        let webReceiverManager = WebReceiverManager()
+
+        return AppABI(
+            apiManager: nil,
+            appConfiguration: appConfiguration,
+            appEncoder: appEncoder,
+            configManager: configManager,
+            extensionInstaller: nil,
+            iapManager: iapManager,
+            kvStore: kvStore,
+            logFormatter: logFormatter,
+            preferencesManager: nil,
+            profileManager: profileManager,
+            registry: registry,
+            tunnelManager: tunnelManager,
+            versionChecker: versionChecker,
+            webReceiverManager: webReceiverManager
+        )
+    }
+}
+#endif
