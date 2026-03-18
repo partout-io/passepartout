c09267083f96c72b7e1355e9b53c938d79ef7647
Davide <keeshux@gmail.com>
2026-03-18 20:46:27 +0100

    [ci skip]

--- app-android/app/src/main/java/com/algoritmico/passepartout/MainActivity.kt
+++ app-android/app/src/main/java/com/algoritmico/passepartout/MainActivity.kt
@@ -65,7 +65,7 @@ class MainActivity : ComponentActivity(), ABIEventCallback {
         )
 
         val constantsJSON: AppConstants = json.decodeFromString(constants)
-        Log.e("Passepartout", "${constantsJSON.github.discussions}")
+        Log.e("Passepartout", ">>> Test GitHub constants: ${constantsJSON.github.discussions}")
 
         setContent {
             HelloWorldView(
