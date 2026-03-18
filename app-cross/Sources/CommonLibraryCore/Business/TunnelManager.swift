partout-io/passepartout

commit 9b5cdcfc79ce66cbb88e9cc3a322e0b26a1e5327

Do not report .dataCount if no active tunnels (#1716)

app-cross/Sources/CommonLibraryCore/Business/TunnelManager.swift
@@ -188,6 +188,7 @@ private extension TunnelManager {
                     break
                 }
                 latestEnvironments = await tunnel.allEnvironments()
+                guard !latestEnvironments.isEmpty else { return }
                 didChange.send(.dataCount())
                 try? await Task.sleep(interval: interval)
             }

commit 113037df89c8ad3ca3854db17919cf01646b337a

Fix unintended exit in .dataCount observer
Regression in #1716

app-cross/Sources/CommonLibraryCore/Business/TunnelManager.swift
@@ -187,8 +187,9 @@ extension TunnelManager {
                     break
                 }
                 latestEnvironments = await tunnel.allEnvironments()
-                guard !latestEnvironments.isEmpty else { return }
-                didChange.send(.dataCount())
+                if !latestEnvironments.isEmpty {
+                    didChange.send(.dataCount())
+                }
                 try? await Task.sleep(interval: interval)
             }
         }