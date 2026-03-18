e140603e7acad2640fd3107b2064710873eefef6
Davide <keeshux@gmail.com>
2026-03-18 18:05:44 +0100

    Merge PreferencesProtocol

--- app-cross/Sources/CommonLibraryCore/Domain/PreferenceProtocol.swift
+++
@@ -1,9 +0,0 @@
-// SPDX-FileCopyrightText: 2026 Davide De Rosa
-//
-// SPDX-License-Identifier: GPL-3.0
-
-extension ABI {
-    public protocol PreferenceProtocol {
-        var key: String { get }
-    }
-}
