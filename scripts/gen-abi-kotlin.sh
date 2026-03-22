8cbcdcc9a0aa96ff9e4256cdbab551bfca6b1860
Davide <keeshux@gmail.com>
2026-03-22 15:13:50 +0100

    Do not depend on Partout scripts for codegen

--- scripts/gen-abi-kotlin.sh
+++ scripts/gen-abi-kotlin.sh
@@ -2,27 +2,39 @@
 cwd=`dirname $0`
 source $cwd/env.sh
 cd $cwd/..
+repo_root="$(pwd)"
+codegen="$repo_root/node_modules/.bin/openapi-generator-cli"
 
 set -e
-infile=app-cross/abi.yaml
-models_dir=`realpath app-android/app`
+partout_infile=app-cross/partout/scripts/openapi.yaml
+partout_package=io.partout.abi
+abi_infile=app-cross/abi.yaml
 abi_package=com.algoritmico.passepartout.abi
+models_dir=`realpath app-android/app`
 
 # Clean up
 rm -rf $models_dir/src/main/kotlin/io/partout/abi
 rm -rf $models_dir/src/main/kotlin/com/algoritmico/passepartout/abi
 
+shared_flags=(
+    -o "$models_dir"
+    -g kotlin
+    --global-property=models,modelDocs=false,modelTests=false
+    --type-mappings number=Double,URI=String
+    --import-mappings Double=kotlin.Double,String=kotlin.String
+    --additional-properties=serializationLibrary=kotlinx_serialization
+)
+
 # Generate Partout entities
-( cd app-cross/partout && scripts/gen-models.sh kotlin $models_dir )
+"$codegen" generate \
+    -i "$partout_infile" \
+    --additional-properties=packageName=$partout_package \
+    --additional-properties=modelPackage=$partout_package \
+    "${shared_flags[@]}"
 
 # Generate Passepartout ABI entities
-$codegen generate \
-    -i $infile \
-    -o $models_dir \
-    -g kotlin \
-    --global-property=models,modelDocs=false,modelTests=false \
-    --type-mappings number=Double,URI=String \
-    --import-mappings Double=kotlin.Double,String=kotlin.String \
-    --additional-properties=serializationLibrary=kotlinx_serialization \
+"$codegen" generate \
+    -i "$abi_infile" \
     --additional-properties=packageName=$abi_package \
-    --additional-properties=modelPackage=$abi_package
+    --additional-properties=modelPackage=$abi_package \
+    "${shared_flags[@]}"
