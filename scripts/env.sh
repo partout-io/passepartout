627da9f7439905d0c520eda360cb195a30c9fc15
Davide <keeshux@gmail.com>
2026-03-22 14:08:52 +0100

    Update Partout

--- scripts/env.sh
+++ scripts/env.sh
@@ -7,4 +7,3 @@ translations_input_path="l10n"
 translations_output_path="app-apple/Sources/AppStrings/Resources"
 build_type=Release
 toolchains_path="app-cross/partout/toolchains"
-schemas_path="app-cross/codegen"
