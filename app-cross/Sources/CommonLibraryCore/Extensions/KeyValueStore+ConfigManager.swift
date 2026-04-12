90873fffd27e64d1ef860d47580288a6c90fca3a
Davide <keeshux@gmail.com>
2026-04-12 19:53:23 +0200

    Fix actor notation not building on non-Apple

--- app-cross/Sources/CommonLibraryCore/Extensions/KeyValueStore+ConfigManager.swift
+++ app-cross/Sources/CommonLibraryCore/Extensions/KeyValueStore+ConfigManager.swift
@@ -3,7 +3,7 @@
 // SPDX-License-Identifier: GPL-3.0
 
 extension KeyValueStore {
-    @MainActor
+    @BusinessActor
     public func constrainRelaxedVerification(to configManager: ConfigManager) {
         guard configManager.isActive(.allowsRelaxedVerification) else {
             set(false, forAppPreference: .relaxedVerification)
