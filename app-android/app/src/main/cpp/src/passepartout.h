1ca390d400e68b2ff80f4ba44afec81475d3173b
Davide <keeshux@gmail.com>
2026-04-30 01:50:02 +0200

    [ci skip] Fix stale Android header

--- app-android/app/src/main/cpp/src/passepartout.h
+++ app-android/app/src/main/cpp/src/passepartout.h
@@ -21,9 +21,9 @@ typedef void (*psp_event_callback)(void *event_ctx, const char *event);
  * - Success: code == 0, data = JSON (optional)
  * - Error:   code != 0, data = String
  */
-#define PSPABICompletionCodeOK          0
-#define PSPABICompletionCodeArgs        -2
-#define PSPABICompletionCodeFailure     -1
+#define PSPCompletionCodeOK         0
+#define PSPCompletionCodeArgs       -2
+#define PSPCompletionCodeFailure    -1
 typedef void (*psp_completion_cb)(void *ctx, int code, const char *data);
 typedef struct {
     void *ctx;
