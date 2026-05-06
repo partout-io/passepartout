ea8ac53f89ec576b6e468d7bca5f85d3ff5d93e3
Davide <keeshux@gmail.com>
2026-05-06 14:37:09 +0200

    Update iperf3 script for both download and upload

--- scripts/speed-test.sh
+++ scripts/speed-test.sh
@@ -1,2 +1,16 @@
 #!/bin/sh
-iperf3-darwin -c 10.12.0.1 -P 8 -t 600 -R
+
+case "$1" in
+    down)
+        DIRECTION_FLAGS="-R"
+        ;;
+    up)
+        DIRECTION_FLAGS=""
+        ;;
+    *)
+        echo "Usage: $0 up|down" >&2
+        exit 1
+        ;;
+esac
+
+iperf3-darwin -c 10.12.0.1 -P 8 -t 600 $DIRECTION_FLAGS
