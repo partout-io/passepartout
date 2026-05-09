658e5b36f00576b9ceb93da46eff53d115e54428
Davide <keeshux@gmail.com>
2026-05-09 22:08:39 +0200

    Delete old build methods
    - Drop manual building from build scripts
    - Drop environment, now in CMake directly
    - Drop pull libraries script
    - Drop copied header

--- scripts/build.sh
+++ scripts/build.sh
@@ -22,16 +22,6 @@ while [[ $# -gt 0 ]]; do
             build_app=1
             shift
             ;;
-        -android)
-            if [[ ! -d $ANDROID_NDK_HOME ]]; then
-                echo "\$ANDROID_NDK_HOME must point to the Android NDK"
-                exit 1
-            fi
-            source $cwd/env-android.sh
-            for_android=1
-            build_dir=${build_dir}-android
-            shift
-            ;;
     esac
 done
 set -- "${positional_args[@]}"
@@ -41,20 +31,14 @@ if [[ -z $build_type ]]; then
 fi
 cmake_opts+=("-DCMAKE_BUILD_TYPE=$build_type")
 
-if [[ $(uname -s) == "Linux" && $for_android != 1 ]]; then
-    is_linux=1
+if [[ $(uname -s) == "Linux" ]]; then
     source $cwd/env-linux.sh
-fi
-
-if [[ $gen_build == 1 ]]; then
-    if [[ $for_android == 1 ]]; then
-        cmake_opts+=("-DCMAKE_TOOLCHAIN_FILE=$partout_toolchains_path/android.toolchain.cmake")
-    elif [[ $is_linux == 1 ]]; then
+    if [[ $gen_build == 1 ]]; then
         cmake_opts+=("-DCMAKE_TOOLCHAIN_FILE=$partout_toolchains_path/linux.toolchain.cmake")
     fi
 fi
 
-if [[ $build_app == 1 && $for_android != 1 ]]; then
+if [[ $build_app == 1 ]]; then
     cmake_opts+=("-DBUILD_APP=ON")
 fi
 
