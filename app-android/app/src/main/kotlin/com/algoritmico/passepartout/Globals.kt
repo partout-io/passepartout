b70366e68af73e80a1a386d63b026cd9957bb65d
Davide <keeshux@gmail.com>
2026-05-16 14:07:46 +0200

    Move service to sub package
    Reuse readAsset from Globals.

--- app-android/app/src/main/kotlin/com/algoritmico/passepartout/Globals.kt
+++ app-android/app/src/main/kotlin/com/algoritmico/passepartout/Globals.kt
@@ -4,8 +4,13 @@
 
 package com.algoritmico.passepartout
 
+import android.content.Context
 import kotlinx.serialization.json.Json
 
 val globalJsonCoder = Json {
     ignoreUnknownKeys = true
+}
+
+fun Context.readAsset(name: String): String {
+    return assets.open(name).bufferedReader().use { it.readText() }
 }
