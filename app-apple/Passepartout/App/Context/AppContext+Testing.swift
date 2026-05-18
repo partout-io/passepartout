partout-io/passepartout

commit 7580905e93680c75d4920b89bbafe952635d2eb6

ABI: Move Tunnel above the ABI layer (#1816)
Talking to the Partout tunnels natively makes the TunnelObservable
implementations way more natural.

Now:

- TunnelObservable speaks directly to Tunnel and maintains a local
state, both in Swift and Kotlin
- TunnelManager and TunnelEvent are removed from the ABI,
TunnelObservable emits its own UI-level state
- The ABI receives TunnelObservable as high-level tunnel hooks to
perform tunnel operations on internal changes, e.g., to reconnect a
profile when it's modified
- The Android hooks to PartoutTunnel must be provided to the ABI via
NativeTunnelHooks/JNI (regression, see FIXME)

Refactoring:

- Move profile title closures to NETunnelStrategy, removed from:
AppTunnelProcessor, NEProfileRepository
- Relocate Kotlin packages

app-apple/Passepartout/App/Context/AppContext+Testing.swift
@@ -53,9 +53,6 @@ extension AppContext {
             processor: profileProcessor
         )
         profileManager.enableRemoteImporting(true)
-        let tunnel = Tunnel(.global, strategy: FakeTunnelStrategy()) { @Sendable _ in
-            SharedTunnelEnvironment(profileId: nil)
-        }
         let tunnelProcessor = appConfiguration.newAppTunnelProcessor(
             apiManager: apiManager,
             resolver: registry,
@@ -63,10 +60,15 @@ extension AppContext {
                 $0.sort(using: $1.sortingComparators)
             }
         )
-        let tunnelManager = TunnelManager(
-            tunnel: tunnel,
-            processor: tunnelProcessor
+        let tunnel = Tunnel(
+            .global,
+            strategy: FakeTunnelStrategy(),
+            willInstall: tunnelProcessor.willInstall,
+            environmentFactory: { @Sendable _ in
+                SharedTunnelEnvironment(profileId: nil)
+            }
         )
+        let tunnelObservable = TunnelObservable(tunnel: tunnel)
         let configManager = ConfigManager()
         let preferencesManager = PreferencesManager()
         let webReceiverManager = WebReceiverManager()
@@ -84,11 +86,16 @@ extension AppContext {
             preferencesManager: preferencesManager,
             profileManager: profileManager,
             registry: registry,
-            tunnelManager: tunnelManager,
+            tunnelHooks: tunnelObservable,
             versionChecker: versionChecker,
             webReceiverManager: webReceiverManager,
             bindings: nil
         )
-        return AppContext(abi: abi, appConfiguration: appConfiguration, kvStore: kvStore)
+        return AppContext(
+            abi: abi,
+            appConfiguration: appConfiguration,
+            kvStore: kvStore,
+            tunnelObservable: tunnelObservable
+        )
     }
 }

app-apple/Sources/AppLibrary/Previews/AppContext+Previews.swift
@@ -44,13 +44,15 @@ extension AppContext {
                 }
             return ProfileManager(profiles: profiles)
         }()
-        let tunnel = Tunnel(.global, strategy: FakeTunnelStrategy(), updateInterval: 10.0) { @Sendable _ in
+        let tunnel = Tunnel(
+            .global,
+            strategy: FakeTunnelStrategy(),
+            refreshInterval: 10000,
+            willInstall: processor.willInstall
+        ) { @Sendable _ in
             SharedTunnelEnvironment(profileId: nil)
         }
-        let tunnelManager = TunnelManager(
-            tunnel: tunnel,
-            processor: processor
-        )
+        let tunnelObservable = TunnelObservable(tunnel: tunnel)
         let preferencesManager = PreferencesManager()
 
         let dummyReceiver = DummyWebReceiver(url: URL(string: "http://127.0.0.1:9000")!)
@@ -73,12 +75,17 @@ extension AppContext {
             preferencesManager: preferencesManager,
             profileManager: profileManager,
             registry: registry,
-            tunnelManager: tunnelManager,
+            tunnelHooks: tunnelObservable,
             versionChecker: versionChecker,
             webReceiverManager: webReceiverManager,
             bindings: nil
         )
-        return AppContext(abi: abi, appConfiguration: appConfiguration, kvStore: kvStore)
+        return AppContext(
+            abi: abi,
+            appConfiguration: appConfiguration,
+            kvStore: kvStore,
+            tunnelObservable: tunnelObservable
+        )
     }()
 }
 

app-apple/Sources/AppLibrary/Previews/MockAppProcessor.swift
@@ -28,10 +28,6 @@ extension MockAppProcessor: ProfileProcessor {
 }
 
 extension MockAppProcessor: AppTunnelProcessor {
-    func title(for profile: Profile) -> String {
-        "Passepartout.Mock: \(profile.name)"
-    }
-
     func willInstall(_ profile: Profile) throws -> Profile {
         profile
     }

app-cross/Sources/CommonLibrary/ABI/AppABI+Apple.swift
@@ -11,6 +11,11 @@ import CoreData
 import Partout
 
 extension AppABI {
+    public struct Result {
+        public let abi: AppABI
+        public let tunnelObservable: TunnelObservable
+    }
+
     public static func forNetworkExtension(
         appConfiguration: ABI.AppConfiguration,
         kvStore: KeyValueStore,
@@ -20,7 +25,7 @@ extension AppABI {
         webStringsBundle: Bundle?,
         withUITesting: Bool,
         withFakeIAPs: Bool
-    ) -> AppABI {
+    ) -> Result {
         let logFormatter = appConfiguration.newLogFormatter()
         let ctx = pspLogRegister(
             for: .app,
@@ -105,7 +110,7 @@ extension AppABI {
             iapHelper = appConfiguration.newInAppHelper()
             iapReceiptReader = SharedReceiptReader(
                 reader: appConfiguration.newInAppReceiptReader {
-                    // TODO: ###, StoreKit receipt caching
+                    // TODO: #1786, StoreKit receipt caching
                     .uncached
                 }
             )
@@ -132,7 +137,6 @@ extension AppABI {
         // MARK: Profiles and Tunnel (NE)
 
         let appEncoder = AppEncoder(coder: registry, kvStore: kvStore)
-        let tunnelIdentifier = appConfiguration.bundle.bundleString(for: .tunnelId)
         let tunnelProcessor = appConfiguration.newAppTunnelProcessor(
             apiManager: apiManager,
             resolver: registry,
@@ -150,14 +154,8 @@ extension AppABI {
         )
         let backupProfileRepository: ProfileRepository? = nil
 #else
-        let tunnelStrategy = NETunnelStrategy(
-            ctx,
-            bundleIdentifier: tunnelIdentifier,
-            coder: appConfiguration.newNEProtocolCoder(ctx, coder: registry)
-        )
-        let mainProfileRepository = NEProfileRepository(repository: tunnelStrategy) { [weak tunnelProcessor] in
-            tunnelProcessor?.title(for: $0) ?? $0.name
-        }
+        let tunnelStrategy = appConfiguration.newNETunnelStrategy(ctx, coder: registry)
+        let mainProfileRepository = NEProfileRepository(repository: tunnelStrategy)
         let backupProfileRepository = appConfiguration.newBackupProfileRepository(
             encoder: appEncoder,
             model: cdRemoteModel,
@@ -174,15 +172,30 @@ extension AppABI {
             backupRepository: backupProfileRepository,
             mirrorsRemoteRepository: false
         )
-        let tunnel = appConfiguration.newTunnel(ctx, strategy: tunnelStrategy)
-        let sysexManager = appConfiguration.newSystemExtensionManager(
-            tunnelIdentifier: tunnelIdentifier
+        let tunnel = Tunnel(
+            ctx,
+            strategy: tunnelStrategy,
+            refreshInterval: Int(appConfiguration.constants.tunnel.refreshInterval * 1000.0),
+            willInstall: { [weak tunnelProcessor] in
+                try await tunnelProcessor?.willInstall($0) ?? $0
+            },
+            environmentFactory: { @Sendable in
+                appConfiguration.newAppTunnelEnvironment(strategy: tunnelStrategy, profileId: $0)
+            }
         )
-        let tunnelManager = appConfiguration.newTunnelManager(
+        let sysexManager = appConfiguration.newSystemExtensionManager()
+
+        // Provide hooks through observable
+        let logging = TunnelObservable.Logging(
+            maxDebugLogLevel: appConfiguration.constants.log.options.maxDebugLogLevel,
+            sinceLast: appConfiguration.constants.log.sinceLast,
+            formatter: logFormatter
+        )
+        let tunnelObservable = TunnelObservable(
             tunnel: tunnel,
-            extensionInstaller: sysexManager,
             kvStore: kvStore,
-            processor: tunnelProcessor
+            extensionInstaller: sysexManager,
+            logging: logging
         )
 
         // MARK: Preferences (Core Data)
@@ -284,7 +297,7 @@ extension AppABI {
             }
         }
 
-        return AppABI(
+        let abi = AppABI(
             apiManager: apiManager,
             appConfiguration: appConfiguration,
             appEncoder: appEncoder,
@@ -296,12 +309,13 @@ extension AppABI {
             preferencesManager: preferencesManager,
             profileManager: profileManager,
             registry: registry,
-            tunnelManager: tunnelManager,
+            tunnelHooks: tunnelObservable,
             versionChecker: versionChecker,
             webReceiverManager: webReceiverManager,
             onEligibleFeaturesBlock: onEligibleFeaturesBlock,
             bindings: nil
         )
+        return Result(abi: abi, tunnelObservable: tunnelObservable)
     }
 }
 

app-cross/Sources/CommonLibrary/Dependencies/AppConfiguration+Dependencies.swift
@@ -19,9 +19,6 @@ extension ABI.AppConfiguration {
         DefaultAppTunnelProcessor(
             apiManager: apiManager,
             resolver: resolver,
-            title: {
-                String(format: constants.tunnel.profileTitleFormat, $0.name)
-            },
             providerServerSorter: providerServerSorter
         )
     }
@@ -133,29 +130,6 @@ extension ABI.AppConfiguration {
             configBlock: {
                 preferences.enabledFlags()
             }
-        )
-    }
-
-    public func newStandaloneTunnel(
-        _ ctx: PartoutLoggerContext,
-        ref: UnsafeMutableRawPointer?
-    ) -> TunnelProtocol {
-        nonisolated(unsafe) let unsafeRef = ref
-        return NativeTunnel(ctx, ref: unsafeRef)
-    }
-
-    @BusinessActor
-    public func newTunnelManager(
-        tunnel: Tunnel,
-        extensionInstaller: ExtensionInstaller?,
-        kvStore: KeyValueStore,
-        processor: AppTunnelProcessor
-    ) -> TunnelManager {
-        TunnelManager(
-            tunnel: tunnel,
-            extensionInstaller: extensionInstaller,
-            kvStore: kvStore,
-            processor: processor
         )
     }
 
@@ -417,20 +391,32 @@ extension ABI.AppConfiguration {
     }
 
     public func newNEProtocolCoder(_ ctx: PartoutLoggerContext, coder: ProfileCoder) -> NEProtocolCoder {
+        let tunnelIdentifier = bundle.bundleString(for: .tunnelId)
         if bundle.distributionTarget.supportsAppGroups {
             return KeychainNEProtocolCoder(
                 ctx,
-                tunnelBundleIdentifier: bundle.bundleString(for: .tunnelId),
+                tunnelBundleIdentifier: tunnelIdentifier,
                 coder: coder,
                 keychain: AppleKeychain(ctx, group: bundle.bundleString(for: .keychainGroupId))
             )
         } else {
             return ProviderNEProtocolCoder(
                 ctx,
-                tunnelBundleIdentifier: bundle.bundleString(for: .tunnelId),
+                tunnelBundleIdentifier: tunnelIdentifier,
                 coder: coder
             )
         }
+    }
+
+    public func newNETunnelStrategy(_ ctx: PartoutLoggerContext, coder: ProfileCoder) -> NETunnelStrategy {
+        NETunnelStrategy(
+            ctx,
+            bundleIdentifier: bundle.bundleString(for: .tunnelId),
+            coder: newNEProtocolCoder(ctx, coder: coder),
+            title: {
+                String(format: constants.tunnel.profileTitleFormat, $0.name)
+            }
+        )
     }
 
     public func newRequest(for url: URL, cached: Bool) async throws -> Data {
@@ -439,25 +425,14 @@ extension ABI.AppConfiguration {
         return try await URLSession.shared.data(for: request).0
     }
 
-    public func newSystemExtensionManager(tunnelIdentifier: String) -> ExtensionInstaller? {
+    public func newSystemExtensionManager() -> SystemExtensionManager? {
         guard bundle.distributionTarget == .developerID else {
             return nil
         }
         return SystemExtensionManager(
-            identifier: tunnelIdentifier,
+            identifier: bundle.bundleString(for: .tunnelId),
             version: bundle.versionNumber,
             build: bundle.buildNumber
-        )
-    }
-
-    public func newTunnel(_ ctx: PartoutLoggerContext, strategy: TunnelObservableStrategy) -> Tunnel {
-        Tunnel(
-            ctx,
-            strategy: strategy,
-            updateInterval: constants.tunnel.refreshInterval,
-            environmentFactory: { @Sendable in
-                newAppTunnelEnvironment(strategy: strategy, profileId: $0)
-            }
         )
     }
 

app-cross/Sources/CommonLibrary/Dependencies/DefaultAppProcessor.swift
@@ -40,24 +40,16 @@ final class DefaultAppTunnelProcessor: AppTunnelProcessor, Sendable {
 
     private let resolver: Resolver
 
-    private let title: @Sendable (Profile) -> String
-
     private let providerServerSorter: ProviderServerParameters.Sorter
 
     init(
         apiManager: APIManager?,
         resolver: Resolver,
-        title: @escaping @Sendable (Profile) -> String,
         providerServerSorter: @escaping @Sendable ProviderServerParameters.Sorter
     ) {
         self.apiManager = apiManager
         self.resolver = resolver
-        self.title = title
         self.providerServerSorter = providerServerSorter
-    }
-
-    nonisolated func title(for profile: Profile) -> String {
-        title(profile)
     }
 
     nonisolated func willInstall(_ profile: Profile) async throws -> Profile {

commit 1840672e2f3c5ce05f5fe6e7b8c6f8e60f6bca14

Fix regressions in profile processing before connection (#1819)
Several things stopped working after refactoring TunnelObservable.

- Weakly retained AppTunnelProcessor in willInstall() leads to no
preprocessing and no interactive login
- Move business logic to willInstall() hook, out of TunnelObservable
- Move hook from Tunnel to TunnelObservable, to handle the `force`
argument
- Handle extensionInstaller in willInstall(), too

Regressions after #1816

app-apple/Passepartout/App/Context/AppContext+Testing.swift
@@ -56,6 +56,7 @@ extension AppContext {
         let tunnelProcessor = appConfiguration.newAppTunnelProcessor(
             apiManager: apiManager,
             resolver: registry,
+            extensionInstaller: nil,
             providerServerSorter: {
                 $0.sort(using: $1.sortingComparators)
             }
@@ -63,12 +64,14 @@ extension AppContext {
         let tunnel = Tunnel(
             .global,
             strategy: FakeTunnelStrategy(),
-            willInstall: tunnelProcessor.willInstall,
             environmentFactory: { @Sendable _ in
                 SharedTunnelEnvironment(profileId: nil)
             }
         )
-        let tunnelObservable = TunnelObservable(tunnel: tunnel)
+        let tunnelObservable = TunnelObservable(
+            tunnel: tunnel,
+            willInstall: tunnelProcessor.willInstall
+        )
         let configManager = ConfigManager()
         let preferencesManager = PreferencesManager()
         let webReceiverManager = WebReceiverManager()

app-apple/Sources/AppLibrary/Extensions/AppCoordinatorConforming+Extensions.swift
@@ -21,6 +21,7 @@ extension AppCoordinatorConforming {
         } catch ABI.AppError.interactiveLogin {
             onInteractiveLogin(profile) { newProfile in
                 Task {
+                    // Force to not re-present the interactive login
                     await onConnect(newProfile, force: true, verify: verify)
                 }
             }

app-apple/Sources/AppLibrary/Previews/AppContext+Previews.swift
@@ -48,11 +48,14 @@ extension AppContext {
             .global,
             strategy: FakeTunnelStrategy(),
             refreshInterval: 10000,
+            environmentFactory: { @Sendable _ in
+                SharedTunnelEnvironment(profileId: nil)
+            }
+        )
+        let tunnelObservable = TunnelObservable(
+            tunnel: tunnel,
             willInstall: processor.willInstall
-        ) { @Sendable _ in
-            SharedTunnelEnvironment(profileId: nil)
-        }
-        let tunnelObservable = TunnelObservable(tunnel: tunnel)
+        )
         let preferencesManager = PreferencesManager()
 
         let dummyReceiver = DummyWebReceiver(url: URL(string: "http://127.0.0.1:9000")!)

app-apple/Sources/AppLibrary/Previews/MockAppProcessor.swift
@@ -28,7 +28,7 @@ extension MockAppProcessor: ProfileProcessor {
 }
 
 extension MockAppProcessor: AppTunnelProcessor {
-    func willInstall(_ profile: Profile) throws -> Profile {
-        profile
+    func willInstall(_ preProfile: Profile, connect: Bool, force: Bool) throws -> Profile? {
+        nil
     }
 }

app-cross/Sources/CommonLibrary/ABI/AppABI+Apple.swift
@@ -136,10 +136,12 @@ extension AppABI {
 
         // MARK: Profiles and Tunnel (NE)
 
+        let sysexManager = appConfiguration.newSystemExtensionManager()
         let appEncoder = AppEncoder(coder: registry, kvStore: kvStore)
         let tunnelProcessor = appConfiguration.newAppTunnelProcessor(
             apiManager: apiManager,
             resolver: registry,
+            extensionInstaller: sysexManager,
             providerServerSorter: {
                 $0.sort(using: $1.sortingComparators)
             }
@@ -176,14 +178,10 @@ extension AppABI {
             ctx,
             strategy: tunnelStrategy,
             refreshInterval: Int(appConfiguration.constants.tunnel.refreshInterval * 1000.0),
-            willInstall: { [weak tunnelProcessor] in
-                try await tunnelProcessor?.willInstall($0) ?? $0
-            },
             environmentFactory: { @Sendable in
                 appConfiguration.newAppTunnelEnvironment(strategy: tunnelStrategy, profileId: $0)
             }
         )
-        let sysexManager = appConfiguration.newSystemExtensionManager()
 
         // Provide hooks through observable
         let logging = TunnelObservable.Logging(
@@ -194,8 +192,8 @@ extension AppABI {
         let tunnelObservable = TunnelObservable(
             tunnel: tunnel,
             kvStore: kvStore,
-            extensionInstaller: sysexManager,
-            logging: logging
+            logging: logging,
+            willInstall: tunnelProcessor.willInstall
         )
 
         // MARK: Preferences (Core Data)

app-cross/Sources/CommonLibrary/Dependencies/AppConfiguration+Dependencies.swift
@@ -14,11 +14,13 @@ extension ABI.AppConfiguration {
     public func newAppTunnelProcessor(
         apiManager: APIManager?,
         resolver: Resolver,
+        extensionInstaller: ExtensionInstaller?,
         providerServerSorter: @escaping ProviderServerParameters.Sorter
     ) -> AppTunnelProcessor {
         DefaultAppTunnelProcessor(
             apiManager: apiManager,
             resolver: resolver,
+            extensionInstaller: extensionInstaller,
             providerServerSorter: providerServerSorter
         )
     }

app-cross/Sources/CommonLibrary/Dependencies/DefaultAppProcessor.swift
@@ -40,49 +40,88 @@ final class DefaultAppTunnelProcessor: AppTunnelProcessor, Sendable {
 
     private let resolver: Resolver
 
+    private let extensionInstaller: ExtensionInstaller?
+
     private let providerServerSorter: ProviderServerParameters.Sorter
 
     init(
         apiManager: APIManager?,
         resolver: Resolver,
+        extensionInstaller: ExtensionInstaller?,
         providerServerSorter: @escaping @Sendable ProviderServerParameters.Sorter
     ) {
         self.apiManager = apiManager
         self.resolver = resolver
+        self.extensionInstaller = extensionInstaller
         self.providerServerSorter = providerServerSorter
     }
 
-    nonisolated func willInstall(_ profile: Profile) async throws -> Profile {
-        guard let apiManager else {
-            return profile
+    nonisolated func willInstall(
+        _ preProfile: Profile,
+        connect: Bool,
+        force: Bool
+    ) async throws -> Profile? {
+        var profile = preProfile
+
+        // Apply provider preprocessing if APIManager provided
+        if let apiManager {
+            // Apply connection heuristic
+            do {
+                if let builder = profile.activeProviderModule?.moduleBuilder() as? ProviderModule.Builder,
+                   let heuristic = builder.entity?.heuristic {
+                    pspLog(.core, .info, "Apply connection heuristic: \(heuristic)")
+                    profile.activeProviderModule?.entity.map {
+                        pspLog(.core, .info, "\tOld server: \($0.server)")
+                    }
+                    profile = try await profile.withNewServer(using: heuristic, apiManager: apiManager, sort: providerServerSorter)
+                    profile.activeProviderModule?.entity.map {
+                        pspLog(.core, .info, "\tNew server: \($0.server)")
+                    }
+                }
+            } catch {
+                pspLog(.core, .error, "Unable to pick new provider server: \(error)")
+            }
+
+            // Validate provider modules. Do not commit resolved
+            // profile, the tunnel requires the original profile.
+            do {
+                _ = try resolver.resolvedProfile(profile)
+            } catch {
+                pspLog(.core, .error, "Unable to inject provider modules: \(error)")
+                throw error
+            }
         }
 
-        // Apply connection heuristic
-        var newProfile = profile
-        do {
-            if let builder = newProfile.activeProviderModule?.moduleBuilder() as? ProviderModule.Builder,
-               let heuristic = builder.entity?.heuristic {
-                pspLog(.core, .info, "Apply connection heuristic: \(heuristic)")
-                newProfile.activeProviderModule?.entity.map {
-                    pspLog(.core, .info, "\tOld server: \($0.server)")
-                }
-                newProfile = try await profile.withNewServer(using: heuristic, apiManager: apiManager, sort: providerServerSorter)
-                newProfile.activeProviderModule?.entity.map {
-                    pspLog(.core, .info, "\tNew server: \($0.server)")
+        // Trigger user input if profile is interactive
+        if connect {
+            guard !profile.isInteractive || force else {
+                throw ABI.AppError.interactiveLogin
+            }
+        }
+
+        // Install extension before proceeding
+        if let extensionInstaller {
+            if extensionInstaller.currentResult == .success {
+                pspLog(.core, .info, "Extensions: already installed")
+            } else {
+                pspLog(.core, .info, "Extensions: install...")
+                do {
+                    let result = try await extensionInstaller.install()
+                    switch result {
+                    case .success:
+                        break
+                    default:
+                        throw ABI.AppError.systemExtension(result)
+                    }
+                    pspLog(.core, .info, "Extensions: installation result is \(result)")
+                } catch {
+                    pspLog(.core, .error, "Extensions: installation error: \(error)")
                 }
             }
-        } catch {
-            pspLog(.core, .error, "Unable to pick new provider server: \(error)")
         }
 
-        // Validate provider modules
-        do {
-            _ = try resolver.resolvedProfile(newProfile)
-            return newProfile
-        } catch {
-            pspLog(.core, .error, "Unable to inject provider modules: \(error)")
-            throw error
-        }
+        // Return processed profile
+        return profile
     }
 }
 

app-cross/Sources/CommonLibraryApple/TunnelObservable.swift
@@ -24,13 +24,15 @@ public final class TunnelObservable: TunnelHooksProtocol {
         public static nonisolated let appPreferences = "appPreferences"
     }
 
+    public typealias WillInstallBlock = @Sendable (Profile, Bool, Bool) async throws -> Profile?
+
     private let tunnel: Tunnel
 
     private let kvStore: KeyValueStore?
 
-    private let extensionInstaller: ExtensionInstaller?
-
     private let logging: Logging?
+
+    private let willInstall: WillInstallBlock?
 
     public private(set) var activeProfiles: [Profile.ID: ABI.AppTunnelInfo]
 
@@ -40,13 +42,13 @@ public final class TunnelObservable: TunnelHooksProtocol {
     public init(
         tunnel: Tunnel,
         kvStore: KeyValueStore? = nil,
-        extensionInstaller: ExtensionInstaller? = nil,
-        logging: Logging? = nil
+        logging: Logging? = nil,
+        willInstall: WillInstallBlock? = nil
     ) {
         self.tunnel = tunnel
         self.kvStore = kvStore
-        self.extensionInstaller = extensionInstaller
         self.logging = logging
+        self.willInstall = willInstall
         activeProfiles = [:]
         subscriptions = []
     }
@@ -69,50 +71,13 @@ extension TunnelObservable {
         try await installAndConnect(true, with: profile, force: force)
     }
 
-//    public func connect(to profileId: Profile.ID, force: Bool) async throws {
-//        guard let profile = profile(withId: profileId) else {
-//            throw ABI.AppError.notFound
-//        }
-//        try await connect(to: profile, force: force)
-//    }
-
-    private func installAndConnect(_ connect: Bool, with profile: Profile, force: Bool) async throws {
-        if connect && !force && profile.isInteractive {
-            throw ABI.AppError.interactiveLogin
-        }
-#if !PSP_CROSS
+    private func installAndConnect(_ connect: Bool, with preProfile: Profile, force: Bool) async throws {
+        let profile = try await willInstall?(preProfile, connect, force) ?? preProfile
         var options: [String: NSObject] = [Options.isManualKey: true as NSNumber]
         if let preferences = kvStore?.preferences {
             let encodedPreferences = try ABI.encode(preferences)
             options[Options.appPreferences] = encodedPreferences as NSData
         }
-#else
-        // Cross sends no .isManualKey to startTunnel()
-        var options: Sendable?
-#endif
-
-#if os(macOS)
-        if let extensionInstaller {
-            if extensionInstaller.currentResult == .success {
-                pspLog(.core, .info, "Extensions: already installed")
-            } else {
-                pspLog(.core, .info, "Extensions: install...")
-                do {
-                    let result = try await extensionInstaller.install()
-                    switch result {
-                    case .success:
-                        break
-                    default:
-                        throw ABI.AppError.systemExtension(result)
-                    }
-                    pspLog(.core, .info, "Extensions: installation result is \(result)")
-                } catch {
-                    pspLog(.core, .error, "Extensions: installation error: \(error)")
-                }
-            }
-        }
-#endif
-
         try await tunnel.install(profile, connect: connect, options: options)
     }
 

app-cross/Sources/CommonLibraryCore/Strategy/Processors.swift
@@ -11,7 +11,11 @@ public protocol ProfileProcessor: AnyObject, Sendable {
 }
 
 public protocol AppTunnelProcessor: AnyObject, Sendable {
-    nonisolated func willInstall(_ profile: Profile) async throws -> Profile
+    nonisolated func willInstall(
+        _ preProfile: Profile,
+        connect: Bool,
+        force: Bool
+    ) async throws -> Profile?
 }
 
 public protocol PacketTunnelProcessor: AnyObject, Sendable {

app-cross/Tests/CommonLibraryTests/Mock/MockTunnelProcessor.swift
@@ -1,21 +1,0 @@
-// SPDX-FileCopyrightText: 2026 Davide De Rosa
-//
-// SPDX-License-Identifier: GPL-3.0
-
-import Partout
-
-final class MockTunnelProcessor: AppTunnelProcessor, @unchecked Sendable {
-    var titleCount = 0
-
-    var willInstallCount = 0
-
-    func title(for profile: Profile) -> String {
-        titleCount += 1
-        return ""
-    }
-
-    func willInstall(_ profile: Profile) throws -> Profile {
-        willInstallCount += 1
-        return profile
-    }
-}