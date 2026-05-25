7681840f169720bb3180a6eb540dd83e36b3af5e
Davide <keeshux@gmail.com>
2026-05-25 23:48:08 +0200

    CMake: Fix compile issue

--- app-cross/passepartout_shared.cmake
+++ app-cross/passepartout_shared.cmake
@@ -172,8 +172,8 @@ Sources/CommonLibraryCore/Domain/StoreReceipt.swift
 Sources/CommonLibraryCore/Domain/StoreResult.swift
 Sources/CommonLibraryCore/Domain/VersionRelease.swift
 Sources/CommonLibraryCore/Extensions/ABI+JSON.swift
-Sources/CommonLibraryCore/Extensions/AppPreferences+CRUD.swift
 Sources/CommonLibraryCore/Extensions/AppPreferences+ConfigManager.swift
+Sources/CommonLibraryCore/Extensions/AppPreferences+Encoding.swift
 Sources/CommonLibraryCore/Extensions/ChangelogEntry+Extensions.swift
 Sources/CommonLibraryCore/Extensions/CodingRegistry+Extensions.swift
 Sources/CommonLibraryCore/Extensions/Module+Extensions.swift
