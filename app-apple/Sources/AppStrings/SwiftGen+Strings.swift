58396f15532f306b1434396b9133f2357b9428da
Davide <keeshux@gmail.com>
2026-05-27 18:32:57 +0200

    [ci skip] Omit 'file' on parsing errors

--- app-apple/Sources/AppStrings/SwiftGen+Strings.swift
+++ app-apple/Sources/AppStrings/SwiftGen+Strings.swift
@@ -143,8 +143,8 @@ public enum Strings {
         public static let missingProviderEntity = Strings.tr("Localizable", "errors.app.passepartout.missing_provider_entity", fallback: "No server selected in provider.")
         /// The profile has no active modules.
         public static let noActiveModules = Strings.tr("Localizable", "errors.app.passepartout.no_active_modules", fallback: "The profile has no active modules.")
-        /// Unable to parse file.
-        public static let parsing = Strings.tr("Localizable", "errors.app.passepartout.parsing", fallback: "Unable to parse file.")
+        /// Unable to parse.
+        public static let parsing = Strings.tr("Localizable", "errors.app.passepartout.parsing", fallback: "Unable to parse.")
         /// The operation timed out.
         public static let timeout = Strings.tr("Localizable", "errors.app.passepartout.timeout", fallback: "The operation timed out.")
         public enum Openvpn {
