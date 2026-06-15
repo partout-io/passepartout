7867356645ffcdf898c6224321aff0ca13dda3cb
Davide <keeshux@gmail.com>
2026-06-15 17:17:34 +0200

    Migrate app-shared CMake conf to app-cross

--- scripts/gen-cmake-files.sh
+++ scripts/gen-cmake-files.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
 LC_ALL=C
-libpassepartout=passepartout_shared.cmake
 passepartout=passepartout.cmake
-
-pushd app-shared
-cat >${libpassepartout} <<EOF
-set(PSP_SOURCES
-$(find Sources -name "*.swift" | sort)
-)
-set(PSP_C_SOURCES
-$(find Sources -name "*.c" | sort)
-)
-EOF
-popd
-
 pushd app-cross
 cat >${passepartout} <<EOF
 set(APP_SOURCES
