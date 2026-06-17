f69b836cfd3c37e25cb60550c7118858485b831a
Davide <keeshux@gmail.com>
2026-06-17 18:54:20 +0200

    [ci skip] Append underlying reason

--- app-android/app/src/main/kotlin/com/algoritmico/passepartout/ui/extensions/AppError+UI.kt
+++ app-android/app/src/main/kotlin/com/algoritmico/passepartout/ui/extensions/AppError+UI.kt
@@ -6,12 +6,21 @@ package com.algoritmico.passepartout.ui.extensions
 
 import com.algoritmico.passepartout.observables.AppError
 import com.algoritmico.passepartout.observables.ProfileImporterException
+import com.algoritmico.passepartout.observables.asAppError
 
 // FIXME: ###, AppError, Localize messages
 val AppError.localizedMessage: String
-    get() = when (cause) {
-        is ProfileImporterException.Binary -> "Importing a binary file."
-        is ProfileImporterException.Failure -> "Unable to import profile: ${cause.cause}"
-        is ProfileImporterException.Null -> "Unable to read profile content"
-        else -> cause?.localizedMessage ?: code.name
+    get() {
+        var msg = when (cause) {
+            is ProfileImporterException.Binary -> "Importing a binary file."
+            is ProfileImporterException.Failure -> "Unable to import profile."
+            is ProfileImporterException.Null -> "Unable to read profile content."
+            else -> cause?.asAppError?.localizedMessage ?: code.name
+        }
+        val underlyingCause = cause?.cause
+        underlyingCause?.asAppError?.let {
+            msg += " "
+            msg += it.localizedMessage
+        }
+        return msg
     }
