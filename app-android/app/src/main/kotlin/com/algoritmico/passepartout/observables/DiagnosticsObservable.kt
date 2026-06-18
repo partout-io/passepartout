6a222071176fced8acc0d0464a09897a38e842e3
Davide <keeshux@gmail.com>
2026-06-18 18:07:09 +0200

    Build issue with logs in DiagnosticsObservable

--- app-android/app/src/main/kotlin/com/algoritmico/passepartout/observables/DiagnosticsObservable.kt
+++ app-android/app/src/main/kotlin/com/algoritmico/passepartout/observables/DiagnosticsObservable.kt
@@ -4,8 +4,14 @@
 
 package com.algoritmico.passepartout.observables
 
+import android.content.Context
 import com.algoritmico.passepartout.business.extensions.runCatchingNonFatal
+import com.algoritmico.passepartout.business.extensions.versionString
+import com.algoritmico.passepartout.context.Tags
 import com.algoritmico.passepartout.context.LocalConstants
+import com.algoritmico.passepartout.context.androidSystemInformation
+import com.algoritmico.passepartout.models.AppConfiguration
+import com.algoritmico.passepartout.models.Issue
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.coroutineScope
@@ -13,9 +19,35 @@ import kotlinx.coroutines.withContext
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
+import java.util.UUID
 import java.util.concurrent.TimeUnit
 
 class DiagnosticsObservable {
+    suspend fun issue(
+        context: Context,
+        appConfiguration: AppConfiguration,
+        comment: String
+    ): Issue = coroutineScope {
+        val appLog = async {
+            logcat(Tags.appTags, LocalConstants.LOGCAT_VIEW_HOURS).toLogData()
+        }
+        val tunnelLog = async {
+            logcat(Tags.serviceTags, LocalConstants.LOGCAT_VIEW_HOURS).toLogData()
+        }
+        val systemInformation = context.androidSystemInformation()
+        Issue(
+            id = UUID.randomUUID(),
+            comment = comment,
+            appLine = "${appConfiguration.bundle.displayName} ${appConfiguration.bundle.versionString} [${appConfiguration.bundle.distributionTarget}]",
+            purchasedProducts = emptyList(),
+            providerLastUpdates = emptyMap(),
+            appLog = appLog.await(),
+            tunnelLog = tunnelLog.await(),
+            osLine = systemInformation.osLine,
+            deviceLine = systemInformation.deviceLine
+        )
+    }
+
     suspend fun logcat(
         tags: Collection<String>,
         hours: Long
@@ -80,5 +112,13 @@ class DiagnosticsObservable {
         ) + tags.map {
             "$it:V"
         } + "*:S"
+    }
+
+    private fun List<String>.toLogData(): ByteArray? {
+        if (isEmpty()) {
+            return null
+        }
+        return joinToString(separator = "\n")
+            .toByteArray(Charsets.UTF_8)
     }
 }
