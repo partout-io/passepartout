b105684aa438ad956d025ee049919db2b184d5f5
Davide <keeshux@gmail.com>
2026-06-24 12:47:59 +0200

    Android: Address UI battery usage (#1884)
    Do some tricks to reduce the reported battery usage. Arguably related to
    UI, not the VPN service.

--- app-android/app/src/main/kotlin/com/algoritmico/passepartout/ui/settings/VersionUpdateLink.kt
+++ app-android/app/src/main/kotlin/com/algoritmico/passepartout/ui/settings/VersionUpdateLink.kt
@@ -5,10 +5,10 @@
 package com.algoritmico.passepartout.ui.settings
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.algoritmico.passepartout.R
 import com.algoritmico.passepartout.business.extensions.versionString
 import com.algoritmico.passepartout.ui.LocalErrorHandler
@@ -19,7 +19,7 @@ import com.algoritmico.passepartout.ui.theme.ThemeNavigatingButton
 @Composable
 fun VersionUpdateLink() {
     val versionObservable = LocalVersionObservable.current
-    val state by versionObservable.state.collectAsState()
+    val state by versionObservable.state.collectAsStateWithLifecycle()
     val latestRelease = state.latestRelease ?: return
     val uriHandler = LocalUriHandler.current
     val errorHandler = LocalErrorHandler.current
