47a0c09b8ca16217de0d84a8a3d641313376eb8a
Davide <keeshux@gmail.com>
2026-06-26 10:02:34 +0200

    Reuse toolchains from Partout

--- scripts/env.sh
+++ scripts/env.sh
@@ -5,5 +5,5 @@ metadata_root="fastlane/metadata"
 metadata_path="default/release_notes.txt"
 translations_input_path="l10n"
 translations_output_path="app-apple/Sources/AppStrings/Resources"
-cmake_toolchains_path="cmake"
+cmake_toolchains_path="partout/cmake/swift"
 codegen=$(pwd)/node_modules/.bin/openapi-generator-cli
