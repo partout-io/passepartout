2c8b9eef3236c5bd6d9347172dedd1e5c9c3f885
Davide <keeshux@gmail.com>
2026-06-26 09:30:42 +0200

    Try libc++_shared

--- app-android/app/build.gradle.kts
+++ app-android/app/build.gradle.kts
@@ -22,6 +22,11 @@ android {
             //noinspection ChromeOsAbiSupport
             abiFilters += listOf("arm64-v8a")
         }
+        externalNativeBuild {
+            cmake {
+                arguments += listOf("-DANDROID_STL=c++_shared")
+            }
+        }
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_21
