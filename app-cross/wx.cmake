a9e001eb572892b150318e3fa33f03f2f548b9fb
Davide <keeshux@gmail.com>
2026-06-28 01:56:29 +0200

    Make app-cross a standalone CMake project
    - Add presets to build directly from CMake
    - Support Debug/Release multiconfig (arm64)
    - Move policy where it belongs
    - Set CMake options in preset
    - Set fallback for OUTPUT_DIR to binary dir
    - Do not override vendor source

--- app-cross/wx.cmake
+++ app-cross/wx.cmake
@@ -40,14 +40,24 @@ function(get_windows_wxwidgets_arch OUT_VAR)
         set(WX_PLATFORM "${CMAKE_GENERATOR_PLATFORM}")
     elseif(CMAKE_VS_PLATFORM_NAME)
         set(WX_PLATFORM "${CMAKE_VS_PLATFORM_NAME}")
+    elseif(CMAKE_CXX_COMPILER_ARCHITECTURE_ID)
+        set(WX_PLATFORM "${CMAKE_CXX_COMPILER_ARCHITECTURE_ID}")
+    elseif(CMAKE_C_COMPILER_ARCHITECTURE_ID)
+        set(WX_PLATFORM "${CMAKE_C_COMPILER_ARCHITECTURE_ID}")
+    elseif(CMAKE_CXX_COMPILER_TARGET)
+        set(WX_PLATFORM "${CMAKE_CXX_COMPILER_TARGET}")
+    elseif(CMAKE_C_COMPILER_TARGET)
+        set(WX_PLATFORM "${CMAKE_C_COMPILER_TARGET}")
+    elseif(DEFINED ENV{VSCMD_ARG_TGT_ARCH})
+        set(WX_PLATFORM "$ENV{VSCMD_ARG_TGT_ARCH}")
     else()
         set(WX_PLATFORM "${CMAKE_SYSTEM_PROCESSOR}")
     endif()
     string(TOLOWER "${WX_PLATFORM}" WX_PLATFORM)
 
-    if(WX_PLATFORM STREQUAL "amd64" OR WX_PLATFORM STREQUAL "x86_64" OR WX_PLATFORM STREQUAL "x64")
+    if(WX_PLATFORM MATCHES "^(amd64|x86_64|x64)(-|$)")
         set(${OUT_VAR} "x64" PARENT_SCOPE)
-    elseif(WX_PLATFORM STREQUAL "arm64" OR WX_PLATFORM STREQUAL "aarch64")
+    elseif(WX_PLATFORM MATCHES "^(arm64|aarch64)(-|$)")
         set(${OUT_VAR} "arm64" PARENT_SCOPE)
     else()
         message(FATAL_ERROR "Unsupported wxWidgets Windows prebuilt architecture: ${WX_PLATFORM}")
@@ -100,6 +110,14 @@ function(target_link_windows_wxwidgets TARGET_NAME)
         _UNICODE
         wxDEBUG_LEVEL=0
     )
+    if(MSVC)
+        set_target_properties(${TARGET_NAME} PROPERTIES
+            MSVC_RUNTIME_LIBRARY "MultiThreadedDLL"
+        )
+        target_compile_definitions(${TARGET_NAME} PRIVATE
+            _ITERATOR_DEBUG_LEVEL=0
+        )
+    endif()
     target_link_directories(${TARGET_NAME} PRIVATE "${WX_LIB_DIR}")
     target_link_libraries(${TARGET_NAME} PRIVATE
         wxmsw33u_core
