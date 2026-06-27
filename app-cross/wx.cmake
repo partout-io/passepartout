4801f72f093d09502047cd0ad0377b4b899343e1
Davide <keeshux@gmail.com>
2026-06-27 10:24:23 +0200

    Delete wx submodule, rely on system/prebuilt
    - Mac/Linux: Look up local wx dependency
    - Windows: Fetch prebuilt wx from GitHub

--- app-cross/wx.cmake
+++ app-cross/wx.cmake
@@ -1,150 +1,142 @@
-# wx configuration is highly inconsistent across platforms
-function(target_link_wx_config TARGET_NAME INSTALL_HINT)
-    set(WX_FIND_PROGRAM_ARGS NAMES wx-config)
-    if(ARGN)
-        list(APPEND WX_FIND_PROGRAM_ARGS HINTS ${ARGN})
-    endif()
-    find_program(WX_CONFIG_EXECUTABLE ${WX_FIND_PROGRAM_ARGS})
+set(PASSEPARTOUT_WX_PREBUILT_DIR "" CACHE PATH "Path to an extracted wxWidgets Windows prebuilt.")
+set(PASSEPARTOUT_WX_PREBUILT_BASE_URL
+    "http://github.com/partout-io/prebuilts/releases/latest/download"
+    CACHE STRING
+    "Base URL for wxWidgets Windows prebuilts."
+)
 
-    if(NOT WX_CONFIG_EXECUTABLE)
-        message(FATAL_ERROR "${INSTALL_HINT}")
+function(target_link_system_wxwidgets TARGET_NAME)
+    if(APPLE AND NOT wxWidgets_CONFIG_EXECUTABLE)
+        find_program(wxWidgets_CONFIG_EXECUTABLE
+            NAMES wx-config
+            HINTS /opt/homebrew/bin /usr/local/bin
+            DOC "Location of wxWidgets library configuration provider binary (wx-config)."
+        )
     endif()
 
-    execute_process(
-        COMMAND ${WX_CONFIG_EXECUTABLE} --version
-        OUTPUT_VARIABLE WX_VERSION
-        OUTPUT_STRIP_TRAILING_WHITESPACE
-        RESULT_VARIABLE WX_VERSION_RESULT
-    )
-    if(NOT WX_VERSION_RESULT EQUAL 0)
-        message(FATAL_ERROR "Unable to query wxWidgets version with ${WX_CONFIG_EXECUTABLE}")
+    find_package(wxWidgets 3 REQUIRED COMPONENTS core net base)
+    if(wxWidgets_VERSION)
+        set(WX_VERSION ${wxWidgets_VERSION})
+    else()
+        set(WX_VERSION ${wxWidgets_VERSION_STRING})
     endif()
-    if(NOT WX_VERSION MATCHES "^3\\.")
+    if(WX_VERSION AND NOT WX_VERSION MATCHES "^3\\.")
         message(FATAL_ERROR "Unsupported wxWidgets ${WX_VERSION}. Install a wxWidgets 3.x development package.")
     endif()
-    message(STATUS "Using wxWidgets ${WX_VERSION}: ${WX_CONFIG_EXECUTABLE}")
+    target_link_libraries(${TARGET_NAME} PRIVATE wxWidgets::wxWidgets)
 
-    execute_process(
-        COMMAND ${WX_CONFIG_EXECUTABLE} --cxxflags
-        OUTPUT_VARIABLE WX_CXXFLAGS
-        OUTPUT_STRIP_TRAILING_WHITESPACE
-        RESULT_VARIABLE WX_CXXFLAGS_RESULT
-    )
-    if(NOT WX_CXXFLAGS_RESULT EQUAL 0)
-        message(FATAL_ERROR "Unable to query wxWidgets compiler flags with ${WX_CONFIG_EXECUTABLE}")
-    endif()
-    separate_arguments(WX_CXXFLAGS UNIX_COMMAND "${WX_CXXFLAGS}")
-    target_compile_options(${TARGET_NAME} PRIVATE ${WX_CXXFLAGS})
-
-    execute_process(
-        COMMAND ${WX_CONFIG_EXECUTABLE} --libs core,net,base
-        OUTPUT_VARIABLE WX_LIBS
-        OUTPUT_STRIP_TRAILING_WHITESPACE
-        RESULT_VARIABLE WX_LIBS_RESULT
-    )
-    if(NOT WX_LIBS_RESULT EQUAL 0)
-        message(FATAL_ERROR "Unable to query wxWidgets linker flags with ${WX_CONFIG_EXECUTABLE}")
-    endif()
-    separate_arguments(WX_LIBS UNIX_COMMAND "${WX_LIBS}")
-
-    set(WX_LINK_LIBRARIES)
-    while(WX_LIBS)
-        list(POP_FRONT WX_LIBS WX_LIB)
-        if(WX_LIB STREQUAL "-framework")
-            list(POP_FRONT WX_LIBS WX_FRAMEWORK)
-            list(APPEND WX_LINK_LIBRARIES "-framework ${WX_FRAMEWORK}")
-        else()
-            list(APPEND WX_LINK_LIBRARIES "${WX_LIB}")
+    if(LINUX)
+        find_package(PkgConfig REQUIRED)
+        pkg_check_modules(GTK3 QUIET IMPORTED_TARGET gtk+-3.0)
+        if(NOT GTK3_FOUND)
+            message(FATAL_ERROR "GTK 3 not found. Install the GTK 3 development package, e.g. libgtk-3-dev/libgtk3-dev.")
         endif()
-    endwhile()
-    target_link_libraries(${TARGET_NAME} PRIVATE ${WX_LINK_LIBRARIES})
+        target_link_libraries(${TARGET_NAME} PRIVATE PkgConfig::GTK3 stdc++ m)
+    endif()
 endfunction()
 
-if(APPLE)
-    target_link_wx_config(
-        passepartout
-        "wxWidgets not found. Install it with: brew install wxwidgets"
-        /opt/homebrew/bin
-        /usr/local/bin
-    )
-
-    return()
-endif()
-
-if(LINUX)
-    find_package(PkgConfig REQUIRED)
-    pkg_check_modules(GTK3 QUIET IMPORTED_TARGET gtk+-3.0)
-    if(NOT GTK3_FOUND)
-        message(FATAL_ERROR "GTK 3 not found. Install the GTK 3 development package, e.g. libgtk-3-dev/libgtk3-dev.")
+function(get_windows_wxwidgets_arch OUT_VAR)
+    if(CMAKE_GENERATOR_PLATFORM)
+        set(WX_PLATFORM "${CMAKE_GENERATOR_PLATFORM}")
+    elseif(CMAKE_VS_PLATFORM_NAME)
+        set(WX_PLATFORM "${CMAKE_VS_PLATFORM_NAME}")
+    else()
+        set(WX_PLATFORM "${CMAKE_SYSTEM_PROCESSOR}")
     endif()
-    target_link_wx_config(
-        passepartout
-        "wxWidgets not found. Install a wxGTK 3.x development package, e.g. libwxgtk3.2-dev."
+    string(TOLOWER "${WX_PLATFORM}" WX_PLATFORM)
+
+    if(WX_PLATFORM STREQUAL "amd64" OR WX_PLATFORM STREQUAL "x86_64" OR WX_PLATFORM STREQUAL "x64")
+        set(${OUT_VAR} "x64" PARENT_SCOPE)
+    elseif(WX_PLATFORM STREQUAL "arm64" OR WX_PLATFORM STREQUAL "aarch64")
+        set(${OUT_VAR} "arm64" PARENT_SCOPE)
+    else()
+        message(FATAL_ERROR "Unsupported wxWidgets Windows prebuilt architecture: ${WX_PLATFORM}")
+    endif()
+endfunction()
+
+function(target_link_windows_wxwidgets TARGET_NAME)
+    get_windows_wxwidgets_arch(WX_PREBUILT_ARCH)
+    set(WX_PREBUILT_URL "${PASSEPARTOUT_WX_PREBUILT_BASE_URL}/wxwidgets-windows-${WX_PREBUILT_ARCH}.zip")
+    if(PASSEPARTOUT_WX_PREBUILT_DIR)
+        set(WX_PREBUILT_DIR "${PASSEPARTOUT_WX_PREBUILT_DIR}")
+    else()
+        include(FetchContent)
+        FetchContent_Declare(PassepartoutWxWidgets
+            URL "${WX_PREBUILT_URL}"
+            DOWNLOAD_EXTRACT_TIMESTAMP TRUE
+            SOURCE_SUBDIR cmake-no-add-subdirectory
+        )
+        FetchContent_MakeAvailable(PassepartoutWxWidgets)
+        FetchContent_GetProperties(PassepartoutWxWidgets)
+        set(WX_PREBUILT_DIR "${passepartoutwxwidgets_SOURCE_DIR}")
+    endif()
+    set(WX_LIB_DIR "${WX_PREBUILT_DIR}/lib/vc_${WX_PREBUILT_ARCH}_lib")
+
+    set(WX_REQUIRED_FILES
+        "${WX_PREBUILT_DIR}/include/wx/wx.h"
+        "${WX_LIB_DIR}/mswu/wx/setup.h"
+        "${WX_LIB_DIR}/wxmsw33u_core.lib"
+        "${WX_LIB_DIR}/wxbase33u_net.lib"
+        "${WX_LIB_DIR}/wxbase33u.lib"
     )
-    target_link_libraries(passepartout PRIVATE PkgConfig::GTK3 stdc++ m)
+    foreach(WX_REQUIRED_FILE IN LISTS WX_REQUIRED_FILES)
+        if(NOT EXISTS "${WX_REQUIRED_FILE}")
+            message(FATAL_ERROR
+                "wxWidgets Windows ${WX_PREBUILT_ARCH} prebuilt not found. "
+                "Check ${WX_PREBUILT_URL}, or set PASSEPARTOUT_WX_PREBUILT_DIR "
+                "to an extracted prebuilt directory."
+            )
+        endif()
+    endforeach()
 
-    return()
-endif()
+    message(STATUS "Using wxWidgets Windows ${WX_PREBUILT_ARCH} prebuilt: ${WX_PREBUILT_DIR}")
+    target_include_directories(${TARGET_NAME} PRIVATE
+        "${WX_LIB_DIR}/mswu"
+        "${WX_PREBUILT_DIR}/include"
+    )
+    target_compile_definitions(${TARGET_NAME} PRIVATE
+        __WXMSW__
+        UNICODE
+        _UNICODE
+        wxDEBUG_LEVEL=0
+    )
+    target_link_directories(${TARGET_NAME} PRIVATE "${WX_LIB_DIR}")
+    target_link_libraries(${TARGET_NAME} PRIVATE
+        wxmsw33u_core
+        wxbase33u_net
+        wxbase33u
+        wxjpeg
+        wxpng
+        wxtiff
+        wxzlib
+        wxregexu
+        kernel32
+        user32
+        gdi32
+        gdiplus
+        msimg32
+        comdlg32
+        winspool
+        winmm
+        shell32
+        shlwapi
+        comctl32
+        ole32
+        oleaut32
+        uuid
+        rpcrt4
+        advapi32
+        version
+        ws2_32
+        wininet
+        oleacc
+        uxtheme
+        winhttp
+    )
+endfunction()
 
-# Configure wxWidgets external project
 if(WIN32)
-    set(WX_GENERATOR "Visual Studio 17 2022")
+    target_link_windows_wxwidgets(passepartout)
 else()
-    set(WX_GENERATOR ${CMAKE_GENERATOR})
-endif()
-set(WX_ARGS
-    -DCMAKE_INSTALL_PREFIX=${OUTPUT_DIR}/wx
-    -DwxBUILD_SHARED=OFF
-    -DwxBUILD_TESTS=OFF
-    -DwxBUILD_SAMPLES=OFF
-    -DwxBUILD_DEMOS=OFF
-    -DwxBUILD_PRECOMP=OFF
-    -DwxUSE_LIBWEBP=OFF
-    -DwxUSE_WEBVIEW=OFF
-)
-ExternalProject_Add(wxWidgets
-    SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/wx
-    CMAKE_GENERATOR ${WX_GENERATOR}
-    CMAKE_ARGS ${WX_ARGS}
-    BUILD_COMMAND ""
-    INSTALL_COMMAND ${CMAKE_COMMAND} --build . --target install --config ${CMAKE_BUILD_TYPE}
-)
-
-# Require wxWidgets to build app executable
-add_dependencies(passepartout wxWidgets)
-target_compile_options(passepartout PRIVATE
-    -D_FILE_OFFSET_BITS=64
-    -DwxDEBUG_LEVEL=0
-)
-target_link_directories(passepartout PRIVATE
-    ${OUTPUT_DIR}/wx/lib
-)
-if(WIN32)
-    target_include_directories(passepartout PRIVATE
-        ${OUTPUT_DIR}/wx/include
-        ${OUTPUT_DIR}/wx/include/msvc
-    )
-    target_compile_options(passepartout PRIVATE
-        -D__WXMSW__
-        -D_UNICODE
-    )
-    # MSVC complicates things as usual
-    # - arm64 -> arm64
-    # - amd64 -> x64
-    string(TOLOWER ${CMAKE_SYSTEM_PROCESSOR} WX_VC_ARCH)
-    if(WX_VC_ARCH STREQUAL "amd64")
-        set(WX_VC_ARCH "x64")
-    endif()
-    target_link_directories(passepartout PRIVATE
-        ${OUTPUT_DIR}/wx/lib/vc_${WX_VC_ARCH}_lib
-    )
-    # Append "d" suffix on Windows if Debug
-    if(CMAKE_BUILD_TYPE STREQUAL "Debug")
-        set(WXD "d")
-    endif()
-    target_link_libraries(passepartout PRIVATE
-        wxmsw33u${WXD}_core
-        wxbase33u${WXD}_net
-        wxbase33u${WXD}
-    )
+    target_link_system_wxwidgets(passepartout)
 endif()
