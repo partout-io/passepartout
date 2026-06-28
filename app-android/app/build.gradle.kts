66740d4fa83c168a446ce3ca4cded50ff6bc0ce8
Davide <keeshux@gmail.com>
2026-06-28 15:29:47 +0200

    Bump version

--- app-android/app/build.gradle.kts
+++ app-android/app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "com.algoritmico.passepartout"
         minSdk = 28
         targetSdk = 36
-        versionCode = 4097
+        versionCode = 4098
         versionName = "3.9.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
