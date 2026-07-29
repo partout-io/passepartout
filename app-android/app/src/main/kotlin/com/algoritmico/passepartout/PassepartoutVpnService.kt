bc0ca8e9bd1ccf2951899a0b131230817769b00b
Davide <keeshux@gmail.com>
2026-07-29 09:52:49 +0200

    Drop redundant

--- app-android/app/src/main/kotlin/com/algoritmico/passepartout/PassepartoutVpnService.kt
+++ app-android/app/src/main/kotlin/com/algoritmico/passepartout/PassepartoutVpnService.kt
@@ -160,7 +160,7 @@ class PassepartoutVpnService: VpnService() {
             AppLog.e(logTag, "Unable to start service in foreground", it)
             return START_NOT_STICKY
         }
-        if (VpnService.prepare(applicationContext) != null) {
+        if (prepare(applicationContext) != null) {
             AppLog.w(logTag, "VPN permission was revoked before start")
             runtime.onRevoke()
             return START_NOT_STICKY
