f1cc458c00bc5beb6f0820955437fa9bc9f2714c
Davide <keeshux@gmail.com>
2026-07-30 14:44:09 +0200

    Lint

--- app-apple/Tests/CommonLibraryTests/Domain/AppPreferencesTests.swift
+++ app-apple/Tests/CommonLibraryTests/Domain/AppPreferencesTests.swift
@@ -51,7 +51,7 @@ struct AppPreferencesTests {
 
     @Test
     func givenExperimental_whenDecodeWithoutEnabledFlags_thenUsesEmptySet() throws {
-        let data = #"{"ignoredConfigFlags":["ovpnV3"]}"#.data(using: .utf8)!
+        let data = Data(#"{"ignoredConfigFlags":["ovpnV3"]}"#)
         let sut = try ABI.decode(ABI.ExperimentalPreferences.self, from: data)
         #expect(sut.ignoredConfigFlags == [.ovpnV3])
         #expect(sut.enabledConfigFlags.isEmpty)
