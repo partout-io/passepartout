8af8751f220dbd2c2ea21e003d2664553a33a4ab
Davide <keeshux@gmail.com>
2026-08-24 16:35:56 +0200

    Do not build full

--- app-apple/scripts/prebuild-zig-xcframework.sh
+++ app-apple/scripts/prebuild-zig-xcframework.sh
@@ -8,5 +8,4 @@ script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)
 partout_dir=$(cd "$script_dir/../../partout" && pwd -P)
 "$partout_dir/scripts/build-xcframework.sh" \
     "$script_dir/../PartoutNative.xcframework" \
-    "$partout_dir/prebuilts" \
-    --full
+    "$partout_dir/prebuilts"
