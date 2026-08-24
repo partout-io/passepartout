fc985c38ca3108f8b4a9bf0ae32044221650fb1a
Davide <keeshux@gmail.com>
2026-08-24 14:07:02 +0200

    Skip prebuild in CI

--- app-apple/scripts/prebuild-zig-xcframework.sh
+++ app-apple/scripts/prebuild-zig-xcframework.sh
@@ -2,6 +2,8 @@
 
 set -euo pipefail
 
+[[ -z ${CI:-} ]] || exit 0
+
 script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)
 partout_dir=$(cd "$script_dir/../../partout" && pwd -P)
 [[ -d "$partout_dir/PartoutNative.xcframework" ]] || exit 0
