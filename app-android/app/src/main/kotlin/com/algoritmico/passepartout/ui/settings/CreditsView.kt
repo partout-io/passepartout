11e61b1b93c738ea519318349d5aaf9c315b272f
Davide <keeshux@gmail.com>
2026-09-04 00:04:49 +0200

    Android: Pad credits title

--- app-android/app/src/main/kotlin/com/algoritmico/passepartout/ui/settings/CreditsView.kt
+++ app-android/app/src/main/kotlin/com/algoritmico/passepartout/ui/settings/CreditsView.kt
@@ -8,8 +8,10 @@ import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -246,6 +248,9 @@ private fun LicenseView(
                 )
             }
         } else {
+            item {
+                Spacer(modifier = Modifier.height(theme.spacing.large))
+            }
             items(content.lines()) { line ->
                 Text(
                     text = line,
