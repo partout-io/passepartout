ba785c256d12e9224f1982e20dda917089f0d2a0
Davide <keeshux@gmail.com>
2026-09-25 10:21:50 +0200

    Restore provider alert

--- app-apple/Sources/AppLibrary/Extensions/AppCoordinatorConforming+Extensions.swift
+++ app-apple/Sources/AppLibrary/Extensions/AppCoordinatorConforming+Extensions.swift
@@ -11,6 +11,12 @@ extension AppCoordinatorConforming {
             if verify {
                 try iapObservable.verify(profile, extra: nil)
             }
+            if profile.modules.contains(where: { $0.moduleType == .Custom }) {
+                onInfo(
+                    title: Strings.Global.Nouns.providers,
+                    message: Strings.Onboarding.Migrate395Providers.message
+                )
+            }
             try await tunnel.connect(to: profile, force: force)
         } catch {
             let appError = ABI.AppError(error)
